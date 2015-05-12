34f049e35ad550afc1973c84e721168c8bbff1b4
Xin Qian <last.qx@gmail.com>
2015-05-12 16:59:23 -0400

    add 2d check for the MC truth information

--- src/TruthToyTiling.cxx
+++ src/TruthToyTiling.cxx
@@ -7,27 +7,29 @@ WireCell2dToy::TruthToyTiling::TruthToyTiling(WireCell2dToy::ToyTiling& tiling, 
     if (pvv[itruth].first.x == tbin){
       const Point& p = pvv[itruth].first; // get the point
       float charge = pvv[itruth].second;
-      
-      const GeomWire* wire_u = gds.closest(p, static_cast<WirePlaneType_t>(0));
-      const GeomWire* wire_v = gds.closest(p, static_cast<WirePlaneType_t>(1));
-      const GeomWire* wire_w = gds.closest(p, static_cast<WirePlaneType_t>(2));
 
-      GeomWireSelection wires;
-      wires.push_back(wire_u);
-      wires.push_back(wire_v);
-      wires.push_back(wire_w);
-
-      const GeomCell* cell = tiling.cell(wires);
-
-      if (cell!=0){
-	//Point pp = cell->center();
-	//	std::cout << pp.x << " " << pp.y << " " << pp.z << std::endl;
+      if (gds.contained_yz(p)){
+	const GeomWire* wire_u = gds.closest(p, static_cast<WirePlaneType_t>(0));
+	const GeomWire* wire_v = gds.closest(p, static_cast<WirePlaneType_t>(1));
+	const GeomWire* wire_w = gds.closest(p, static_cast<WirePlaneType_t>(2));
 	
-	if (cellchargemap.find(cell) == cellchargemap.end()){
-	  //not found
-	  cellchargemap[cell] = charge;
-	}else{
-	  cellchargemap[cell] += charge;
+	GeomWireSelection wires;
+	wires.push_back(wire_u);
+	wires.push_back(wire_v);
+	wires.push_back(wire_w);
+	
+	const GeomCell* cell = tiling.cell(wires);
+	
+	if (cell!=0){
+	  //Point pp = cell->center();
+	  //	std::cout << pp.x << " " << pp.y << " " << pp.z << std::endl;
+	  
+	  if (cellchargemap.find(cell) == cellchargemap.end()){
+	    //not found
+	    cellchargemap[cell] = charge;
+	  }else{
+	    cellchargemap[cell] += charge;
+	  }
 	}
       }
     }
