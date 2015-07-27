cfb5f4c4c8439f241f23931bea7c69ab22c2da30
Xin Qian <last.qx@gmail.com>
2015-07-27 15:33:05 -0400

    start to work on the crawler

--- src/ToyCrawler.cxx
+++ src/ToyCrawler.cxx
@@ -3,6 +3,26 @@
 using namespace WireCell;
 
 WireCell2dToy::ToyCrawler::ToyCrawler(MergeSpaceCellSelection& mcells){
+  // form associations ...
+  for (int i = 0; i!=mcells.size();i++){
+    MergeSpaceCell *mcell1 = mcells.at(i);
+    Point m1center = mcell1->Get_Center();
+    double thickness = mcell1->thickness();
+    
+    MergeSpaceCellSelection mcell1_sel;
+    for (int j=0;j!=mcells.size();j++){
+      MergeSpaceCell *mcell2 = mcells.at(j);
+      Point m2center = mcell2->Get_Center();
+
+      if (fabs(m1center.x - m2center.x)==thickness){
+	mcell1_sel.push_back(mcell2);
+      }
+      
+      
+    }
+    mcells_map[mcell1] = mcell1_sel;
+  }
+  
 }
 
 WireCell2dToy::ToyCrawler::~ToyCrawler(){
