af60efc7165025e8344fb17df813070399437a4d
Xin Qian <last.qx@gmail.com>
2015-08-12 14:18:15 -0400

    prepare for tracking

--- src/ToyTracking.cxx
+++ src/ToyTracking.cxx
@@ -2,9 +2,18 @@
 
 using namespace WireCell;
 WireCell2dToy::ToyTracking::ToyTracking(WireCell2dToy::ToyCrawler& toycrawler){
-  
+  for (int i=0;i!=toycrawler.Get_allMCT().size();i++){
+    WCTrack *track = new WCTrack(*toycrawler.Get_allMCT().at(i));
+    tracks.push_back(track);
+  }
+
 }
 
 WireCell2dToy::ToyTracking::~ToyTracking(){
-
+  for (int i=0;i!=tracks.size();i++){
+    delete tracks.at(i);
+  }
+  for (int i=0;i!=vertices.size();i++){
+    delete vertices.at(i);
+  }
 }
