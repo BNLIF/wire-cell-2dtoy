19a011976a51b9d1d6d2c51c80843e1306780e9a
Xin Qian <last.qx@gmail.com>
2015-08-12 13:01:50 -0400

    prepare for tracking

--- inc/WireCell2dToy/ToyTracking.h
+++ inc/WireCell2dToy/ToyTracking.h
@@ -6,12 +6,16 @@
 #include "WireCell2dToy/ToyCrawler.h"
 
 namespace WireCell2dToy{
+  typedef std::map<WireCell::WCTrack*, WireCell::WCVertexSelection> WCT_WCV_Map;
+  typedef std::map<WireCell::WCVertex*, WireCell::WCTrackSelection> WCV_WCT_Map;
+
   class ToyTracking{
   public:
     ToyTracking(ToyCrawler& toycrawler);
     ~ToyTracking();
   protected: 
-    
+    WCT_WCV_Map wct_wcv_map;
+    WCV_WCT_Map wcv_wct_map;
   };
 }
 
