eceeb3022c197a1c3468bfea2f59377cbbce96a4
Xin Qian <last.qx@gmail.com>
2015-08-12 10:49:54 -0400

    prepare for tracking

---
+++ inc/WireCell2dToy/ToyTracking.h
@@ -0,0 +1,15 @@
+#ifndef WIRECELL_TOYTRACKING_H
+#define WIRECELL_TOYTRACKING_H
+
+#include "WireCellData/WCTrack.h"
+#include "WireCellData/WCVertex.h"
+#include "WireCell2dToy/ToyCrawler.h"
+
+namespace WireCell2dToy{
+  class ToyTracking{
+  public:
+  protected: 
+  };
+}
+
+#endif
