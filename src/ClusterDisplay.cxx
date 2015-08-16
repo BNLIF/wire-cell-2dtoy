36ff18ffc241ba690aeddcf94ed960b321319b79
Xin Qian <last.qx@gmail.com>
2015-08-16 09:37:38 -0400

    small improve

--- src/ClusterDisplay.cxx
+++ src/ClusterDisplay.cxx
@@ -50,7 +50,7 @@ void WireCell2dToy::ClusterDisplay::DrawVertex(WCVertexSelection& vertices, TStr
       }
     }
 
-    if (tracks.size()>=1){
+    if (tracks.size()>1){
       for (int j=0;j!=tracks.size();j++){
     	double ky = vertices.at(i)->get_ky(j);
     	double kz = vertices.at(i)->get_kz(j);
@@ -245,7 +245,7 @@ void WireCell2dToy::ClusterDisplay::DrawCluster(MergeSpaceCellSelection& mcells,
   g1->Draw(option);
 
   //test 
-  // g1->GetXaxis()->SetRangeUser(100.,120);
+  g1->GetXaxis()->SetRangeUser(120.,140);
   // g1->GetYaxis()->SetRangeUser(0,20.);
   // g1->GetZaxis()->SetRangeUser(50,70);
 
