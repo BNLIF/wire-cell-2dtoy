9a021eef4b28601eb69a2041c40b05c20caa1e4c
Xin Qian <last.qx@gmail.com>
2015-08-19 19:39:58 -0400

    add an new application

---
+++ apps/wire-cell-signal-processing.cxx
@@ -0,0 +1,105 @@
+#include "WireCellSst/GeomDataSource.h"
+#include "WireCellSst/ToyuBooNEFrameDataSource.h"
+#include "WireCellSst/ToyuBooNESliceDataSource.h"
+#include "WireCell2dToy/ToyEventDisplay.h"
+#include "WireCell2dToy/ToyTiling.h"
+#include "WireCell2dToy/MergeToyTiling.h"
+#include "WireCell2dToy/TruthToyTiling.h"
+#include "WireCell2dToy/SimpleBlobToyTiling.h"
+
+#include "WireCell2dToy/ToyMatrix.h"
+#include "WireCell2dToy/ToyMatrixExclusive.h"
+#include "WireCell2dToy/ToyMatrixKalman.h"
+#include "WireCell2dToy/ToyMatrixIterate.h"
+#include "WireCell2dToy/ToyMatrixMarkov.h"
+#include "WireCell2dToy/ToyMetric.h"
+#include "WireCell2dToy/BlobMetric.h"
+
+#include "WireCellData/MergeGeomCell.h"
+#include "WireCellData/MergeGeomWire.h"
+
+#include "WireCellData/GeomCluster.h"
+//#include "WireCellNav/SliceDataSource.h"
+
+
+#include "WireCellNav/FrameDataSource.h"
+#include "WireCellNav/SimDataSource.h"
+#include "WireCellNav/SliceDataSource.h"
+#include "WireCellSst/Util.h"
+#include "WireCellData/SimTruth.h"
+#include "WireCell2dToy/ToyDepositor.h"
+#include "WireCellNav/GenerativeFDS.h"
+#include "WireCell2dToy/ToySignalSimu.h"
+#include "WireCell2dToy/ToySignalSimuTrue.h"
+#include "WireCell2dToy/ToySignalGaus.h"
+#include "WireCell2dToy/ToySignalWien.h"
+
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "TGraph2D.h"
+#include "TColor.h"
+#include "TVectorD.h"
+#include "TMatrixD.h"
+#include <iostream>
+
+using namespace WireCell;
+using namespace std;
+
+
+
+
+int main(int argc, char* argv[])
+{
+  if (argc < 4) {
+    cerr << "usage: wire-cell-uboone /path/to/ChannelWireGeometry.txt /path/to/celltree.root eve_num" << endl;
+    return 1;
+  }
+  
+  
+  WireCellSst::GeomDataSource gds(argv[1]);
+  std::vector<double> ex = gds.extent();
+  cerr << "Extent: "
+       << " x:" << ex[0]/units::mm << " mm"
+       << " y:" << ex[1]/units::m << " m"
+       << " z:" << ex[2]/units::m << " m"
+       << endl;
+
+  cout << "Pitch: " << gds.pitch(WirePlaneType_t(0)) 
+       << " " << gds.pitch(WirePlaneType_t(1)) 
+       << " " << gds.pitch(WirePlaneType_t(2))
+       << endl;
+  cout << "Angle: " << gds.angle(WirePlaneType_t(0)) 
+       << " " << gds.angle(WirePlaneType_t(1)) 
+       << " " << gds.angle(WirePlaneType_t(2))
+       << endl;
+  
+  
+  const char* root_file = argv[2];
+  const char* tpath = "/Event/Sim";
+  
+  WireCell::FrameDataSource* fds = 0;
+  fds = WireCellSst::make_fds(root_file);
+  if (!fds) {
+    cerr << "ERROR: failed to get FDS from " << root_file << endl;
+    return 1;
+  }
+  
+  float unit_dis = 1.605723;
+
+  int max_events = 100;
+  int eve_num  = atoi(argv[3]);
+
+  TFile tfile(root_file,"read");
+  TTree* sst = dynamic_cast<TTree*>(tfile.Get(tpath));
+  WireCellSst::ToyuBooNEFrameDataSource data_fds(*sst,gds,9594);
+  data_fds.jump(eve_num);
+  data_fds.Save();
+
+  
+
+  return 0;
+  
+} 
