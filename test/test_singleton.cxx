01119e1b9e069689da723257ab2b7f01afe621db
Xin Qian <last.qx@gmail.com>
2015-11-30 10:04:11 -0500

    add a test for singleton

---
+++ test/test_singleton.cxx
@@ -0,0 +1,36 @@
+#include "WireCell2dToy/Singleton.h"
+//#include "WireCellUtil/Testing.h"
+
+#include <boost/assert.hpp>
+
+#define Assert BOOST_ASSERT
+#define AssertMsg BOOST_ASSERT_MSG 
+
+#include <iostream>
+using namespace std;
+
+class Foo {
+public:
+    Foo() { cout << "Foo() at " << (void*)this << endl; }
+    virtual ~Foo() { cout << "~Foo() at " << (void*)this << endl; }
+    virtual void chirp() { cout << "Foo::chirp() at " << (void*)this << endl; }
+};
+
+typedef WireCell::Singleton<Foo> OnlyFoo;
+
+int main()
+{
+    cout << "First time:" << endl;
+    Foo* foo1 = &OnlyFoo::Instance();
+    Foo* foo2 = &OnlyFoo::Instance();
+
+    Assert(foo1 == foo2);
+
+    OnlyFoo::Instance().chirp();
+    cout << "Second time:" << endl;
+    OnlyFoo::Instance().chirp();
+
+    Foo* foo3 = &WireCell::Singleton<Foo>::Instance();
+    Assert(foo3 == foo1);
+
+}
