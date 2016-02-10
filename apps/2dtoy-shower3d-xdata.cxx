cfd984fdbe0a9036ef65c8a9ffcf4598bbc0beb5
Brett Viren <bv@bnl.gov>
2016-02-10 13:02:08 -0500

    Adapt to Xdata refactor.

--- apps/2dtoy-shower3d-xdata.cxx
+++ apps/2dtoy-shower3d-xdata.cxx
@@ -2,12 +2,14 @@
 
 #include "WireCellData/GeomCell.h"
 
-#include "WireCellXdataRoot/XdataFile.h"
+#include "WireCellXdataRoot/Writer.h"
+#include "WireCellXdataRoot/CloneHelper.h"
 
 #include "TFile.h"
 #include "TTree.h"
 
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -49,120 +51,160 @@ std::pair<int,int> intunpair(uint64_t fs)
     int second = (0xffffffff&fs);
     return make_pair(first,second);
 }
-   
-// grungy reverse mapper from ident to object index in a vector.
-struct IdentMapper {
-    map<uint64_t,int> wire_id2ind;
-    map<uint64_t,int> cell_id2ind; // id is packed wires indices + "context"
-    map<uint64_t,int> blob_id2ind;
-    map<uint64_t,int> deco_id2ind; // id here is a wire index + timeslice
-    
-    Xdata::XdataFile& xdata;
-    uint64_t context;
-    IdentMapper(Xdata::XdataFile& df, uint16_t detector)
-	: xdata(df), context(detector) {}
 
+// A functional object which should be used to map all GeomWires to
+// Xdata:Wires.  Create it with a CloneHelper attached to the
+// TClonesArray for the cells
+// fixme: this probably deserves to be moved into data or nav packages.
+struct WireMapper {
+    unordered_map<int, Xdata::Wire*> wc2xd;
+    Xdata::CloneHelper<Xdata::Wire>& helper;    
 
-    int fill_wire(const GeomWire& gwire) {
-	int ident = gwire.ident();
-	auto it = wire_id2ind.find(ident);
-	if (it != wire_id2ind.end()) {
+    WireMapper(Xdata::CloneHelper<Xdata::Wire>& helper) : helper(helper) {}
+    Xdata::Wire* operator()(const GeomWire* gwire) {
+	auto it = wc2xd.find(gwire->ident());
+	if (it != wc2xd.end()) {
 	    return it->second;
 	}
+	Xdata::Wire* wire = helper.make();
+	wc2xd[gwire->ident()] = wire;
 
-	Xdata::Wire* wire = new Xdata::Wire;
-	wire->wireid = ident;
-	wire->chanid = gwire.channel();
-	wire->volid = gwire.cryo()*1000+gwire.apa(); // pick a random convention
-	wire->plane = gwire.iplane();
-	wire->face = gwire.face();
-	wire->offset = gwire.index();
-	wire->segment = gwire.segment();
-	Point p1 = gwire.point1();
+	wire->ident = gwire->ident();
+	wire->chanid = gwire->channel();
+	wire->offset = gwire->index();
+	wire->plane = (Xdata::planeid_t)gwire->iplane();
+	wire->face = Xdata::kFrontFace;
+	if (gwire->face()) {
+	    wire->face = Xdata::kBackFace;
+	}
+	wire->segment = gwire->segment();
+	Point p1 = gwire->point1();
 	wire->point1 = Xdata::Point(p1.x, p1.y, p1.z);
-	Point p2 = gwire.point2();
+	Point p2 = gwire->point2();
 	wire->point2 = Xdata::Point(p2.x, p2.y, p2.z);
+	wire->apaid = gwire->cryo()*1000+gwire->apa(); // pick a random convention
 
-	int ind = xdata.geom().wires.size();
-	wire_id2ind[ident] = ind;
-	xdata.geom().wires.push_back(wire);
-	return ind;
-    }
-
-    // Some sugar to return the packed cell ID
-    uint64_t cell_ident(const GeomCell& gcell) {
-	return Xdata::Cell::ident_pack(fill_wire(*gcell.get_uwire()),
-				       fill_wire(*gcell.get_vwire()),
-				       fill_wire(*gcell.get_wwire()),
-				       context);
-    }
-
-    int fill_cell(const GeomCell& gcell) {
-	//int ident = gcell.ident();
-	uint64_t ident = cell_ident(gcell);
-	auto it = cell_id2ind.find(ident);
-	if (it != cell_id2ind.end()) {
-	    return it->second;
-	}
-	int ind=-1;
-	Xdata::Cell* cell = xdata.image().new_cell(ind);
-	cell_id2ind[ident] = ind;
-	cell->ident = ident;
-	cell->area = gcell.cross_section();
-	Point p = gcell.center();
-	cell->center = Xdata::Point(p.x,p.y,p.z);
-	return ind;
-    }
-	
-    int fill_deco(const GeomWire& gwire, int time_slice,
-		  const vector<float>& values) {
-	int wind = fill_wire(gwire);
-	uint64_t decoid = intpair(wind, time_slice);
-	auto it = deco_id2ind.find(decoid);
-	if (it != deco_id2ind.end()) {
-	    return it->second;
-	}
-	int ind=-1;
-	Xdata::Deco* deco = xdata.image().new_deco(ind);
-	deco_id2ind[decoid] = ind;
-	deco->wireind = wind;
-	deco->slice = time_slice;
-	deco->values = values;
-	return ind;
-    }
-
-
-    // call this after adding cell
-    int fill_blob(int blob_ident, int cell_ident, int time_slice,
-		  const vector<float>& values) {
-	int cell_index = cell_id2ind[cell_ident];
-	auto it = blob_id2ind.find(blob_ident);
-
-	// no such blob yet
-	if (it == blob_id2ind.end()) { 
-	    int ind = -1;
-	    Xdata::Blob* blob = xdata.image().new_blob(ind);
-	    blob_id2ind[blob_ident] = ind;
-	    blob->ident = blob_ident;
-	    blob->slice = time_slice;
-	    blob->cellind.push_back(cell_index);
-	    blob->values = values;
-	    return ind;
-	}
-	int ind = it->second;
-	Xdata::Blob* blob = xdata.image().get_blob(ind);
-	for (auto maybe_index : blob->cellind) {
-	    if (maybe_index == cell_index) { // already seen this cell
-		return ind;
-	    }
-	}
-	blob->cellind.push_back(cell_index);
-	for (int ival=0; ival<values.size(); ++ival) {
-	    blob->values[ival] += values[ival];
-	}
-	return ind;	
+	return wire;
     }
 };
+
+// A functional object which should be used to map all GeomCells to
+// Xdata::Cells.  Create it with a CloneHelper attached to the
+// TClonesArray for the cells and a WireMapper to handle internal wire
+// mapping.
+// fixme: this probably deserves to be moved into data or nav packages.
+struct CellMapper {
+    unordered_map<int, Xdata::Cell*> wc2xd;
+    Xdata::CloneHelper<Xdata::Cell>& helper;    
+    WireMapper& wiremapper;
+    int tilingid;		// the algorithm that made the cells.
+    
+    CellMapper(Xdata::CloneHelper<Xdata::Cell>& helper,
+	       WireMapper& wiremapper,
+	       int tileingid = 1)
+	: helper(helper)
+	, wiremapper(wiremapper)
+	, tilingid(tilingid)
+	{  }
+
+    Xdata::Cell* operator()(const GeomCell* gcell) {
+	auto it = wc2xd.find(gcell->ident());
+	if (it != wc2xd.end()) { // already have it
+	    return it->second;
+	}
+	Xdata::Cell* cell = helper.make();
+	wc2xd[gcell->ident()] = cell;
+
+	Xdata::Wire* uwire = wiremapper(gcell->get_uwire());
+	Xdata::Wire* vwire = wiremapper(gcell->get_vwire());
+	Xdata::Wire* wwire = wiremapper(gcell->get_wwire());
+	cell->set(uwire->ident, vwire->ident, wwire->ident,tilingid);
+	cell->area = gcell->cross_section();
+	Point p = gcell->center();
+	cell->center = Xdata::Point(p.x,p.y,p.z);
+	return cell;
+    }
+};
+
+// A functional object which should be used to fill  merged cell info into to
+// Xdata::Blobs.  
+// fixme: this probably deserves to be moved into data or nav packages.
+struct BlobMapper {
+    unordered_map<int, Xdata::Blob*> wc2xd; // blob id
+
+    Xdata::CloneHelper<Xdata::Blob>& helper;
+    CellMapper& cellmapper;
+    BlobMapper(Xdata::CloneHelper<Xdata::Blob>& helper,
+	       CellMapper& cellmapper)
+	: helper(helper)
+	, cellmapper(cellmapper)
+	{  }
+    
+    Xdata::Blob* operator()(int blob_id, const GeomCell* gcell,
+			    int time_slice,
+			    float charge) {
+	Xdata::Cell* cell = cellmapper(gcell);
+
+	Xdata::Blob* blob = 0;
+	auto it = wc2xd.find(blob_id);
+	if (it == wc2xd.end()) { // first time
+	    blob = helper.make();
+	    wc2xd[blob_id] = blob;
+	    blob->ident = blob_id;
+	    blob->slice = time_slice;
+	    blob->charge = charge;
+	    blob->cellids.push_back(cell->ident);
+	    return blob;
+	}
+	blob = it->second;
+	for (auto maybe_index : blob->cellids) {
+	    if (maybe_index == cell->ident) { // already seen this cell
+		return blob;
+	    }
+	}
+	if (blob->slice != time_slice) {
+	    cerr << "Corrupt blob data: blob ID=" << blob_id << " cell ID " << cell->ident
+		 << " time slice mismatch: " << blob->slice << " != " << time_slice << endl;
+	    throw runtime_error("data corruption");
+	}
+	// add
+	blob->cellids.push_back(cell->ident);
+	blob->charge += charge;
+	return blob;
+    }
+};
+
+struct DecoMapper {
+
+    unordered_map<uint64_t, Xdata::Deco*> wc2xd; // chan+slice -> deco
+    Xdata::CloneHelper<Xdata::Deco>& helper;
+    WireMapper& wiremapper;
+
+    DecoMapper(Xdata::CloneHelper<Xdata::Deco>& helper,
+	       WireMapper& wiremapper)
+	: helper(helper)
+	, wiremapper(wiremapper)
+	{  }
+
+    Xdata::Deco* operator()(const GeomWire* gwire, int time_slice,
+			    float charge, float uncertainty) {
+	Xdata::Wire* wire = wiremapper(gwire);
+	uint64_t decoid = intpair(wire->chanid, time_slice);
+	auto it = wc2xd.find(decoid);
+	if (it != wc2xd.end()) {
+	    return it->second;
+	}
+	Xdata::Deco* deco = helper.make();
+	wc2xd[decoid] = deco;
+
+	deco->chanid = gwire->channel();
+	deco->slice = time_slice;
+	deco->charge = charge;
+	deco->uncertainty = uncertainty;
+	return deco;
+    }
+};
+
 
 int main(int argc, const char* argv[])
 {
@@ -190,7 +232,7 @@ int main(int argc, const char* argv[])
     runtree->SetBranchAddress("threshold_v", &runobj.threshold_v);
     runtree->SetBranchAddress("threshold_w", &runobj.threshold_w);
     runtree->SetBranchAddress("time_offset", &runobj.time_offset);
-    runtree->GetEntry(0);
+    runtree->GetEntry(0); // get immediately
 
     auto ctree = dynamic_cast<TTree*>(infile->Get("TC"));
     TC cobj;
@@ -242,66 +284,89 @@ int main(int argc, const char* argv[])
     recqtree->SetBranchAddress("ndf",&recqobj.ndf);
 
 
-    Xdata::XdataFile xdatafile;
+    Xdata::Writer xwriter(argv[2]);
 
-    Xdata::RunInfo& ri = xdatafile.runinfo();
+    Xdata::Geom& geom = xwriter.geom.obj();
+    geom.ident = runobj.detector;
+    geom.description = detectors[runobj.detector].c_str();
+    cerr << "Detector: " << geom.description << endl;
+    //geom.wire_bounds = ...;
+    //geom.pitches = ...;
+
+    Xdata::CloneHelper<Xdata::Wire> wireca(*geom.wires);
+    WireMapper wiremap(wireca);
+    Xdata::CloneHelper<Xdata::Cell> cellca(*geom.cells);
+    CellMapper cellmap(cellca, wiremap);
+
+    Xdata::RunInfo& runinfo = xwriter.runinfo.obj();
     uint64_t run64 = runobj.runNo;
     uint64_t subrun64 = runobj.subRunNo;
     uint64_t pack = (run64<<32)|subrun64;
-
-    ri.ident = pack;
+    runinfo.ident = pack;
+    runinfo.geomid = geom.ident;
+    xwriter.runinfo.fill();
     cerr << "Run="  << (pack>>32) << " subrun:" << (0xffffffff&pack) << endl;    
-    ri.detector = detectors[runobj.detector].c_str();
-    cerr << "Detector: " << ri.detector << endl;
 
+    // fixme: in general many triggers will be created.
+    Xdata::Trigger& trigger = xwriter.trigger.obj();
+    trigger.ident = runobj.eventNo;
+    trigger.trigger = 0;	//fixme: this is reason for trigger.
+    trigger.runid = runinfo.ident;
+    trigger.second = 0;		// fixme: nothing real to set here
+    trigger.nanosecond = 0;	// fixme: ibid
+    xwriter.trigger.fill();
 
-    IdentMapper idmap(xdatafile, runobj.detector);
+    // fixme: in general many frames will be created.
+    Xdata::Frame& frame = xwriter.frame.obj();
+    frame.ident = 1;
+    frame.trigid = trigger.ident;
+    frame.toffset = 0;		// fixme: I think this needs to actually be set to something
+    //frame.slicespan = ...;
+    Xdata::CloneHelper<Xdata::Deco> frameca(*frame.decos);
+    DecoMapper decomap(frameca, wiremap);
 
-    Xdata::Image& img = xdatafile.image();
-    img.ident = runobj.eve_num;
-    img.slicespan = runobj.nrebin*0.5;
-
+    // fixme: in general there may be multiple images per frame.
+    Xdata::Image& image = xwriter.image.obj();
+    image.ident = runobj.eve_num;
+    image.frameid = frame.ident;
+    Xdata::CloneHelper<Xdata::Blob> blobca(*image.blobs);
+    BlobMapper blobmap(blobca, cellmap);
     
-    std::chrono::duration<double> in_duration;
-    std::chrono::duration<double> out_duration;
-
+    std::chrono::duration<double> duration1, duration2;
     chrono::time_point<std::chrono::system_clock> clock1, clock2, clock3;
 
     auto nentries = ctree->GetEntries();
     cerr << "Loading " << nentries <<  " entries of TC" << endl;
     for (auto entry=0; entry<nentries; ++entry) {
 	clock1 = chrono::system_clock::now();
+
 	ctree->GetEntry(entry);
+
 	clock2 = chrono::system_clock::now();
 
-	idmap.fill_cell(*geomcell);
-
-	// fixme: is narrowing to float okay?
-	idmap.fill_deco(*geomcell->get_uwire(), cobj.time_slice,
-			vector<float>{(float)cobj.u_charge, (float)cobj.u_charge_err});
-	idmap.fill_deco(*geomcell->get_vwire(), cobj.time_slice,
-			vector<float>{(float)cobj.v_charge, (float)cobj.v_charge_err});
-	idmap.fill_deco(*geomcell->get_wwire(), cobj.time_slice,
-			vector<float>{(float)cobj.w_charge, (float)cobj.w_charge_err});
-
-	// reconstruct blob charge density due to this cell
-	float cell_area = geomcell->cross_section();
-	float charge_density = cobj.charge / cell_area;
-	vector<float> values{cell_area, charge_density};
-
-	idmap.fill_blob(cobj.mcell_id, geomcell->ident(), cobj.time_slice, values);
+	cellmap(geomcell);
+	decomap(geomcell->get_uwire(), cobj.time_slice, cobj.u_charge, cobj.u_charge_err);
+	decomap(geomcell->get_vwire(), cobj.time_slice, cobj.v_charge, cobj.v_charge_err);
+	decomap(geomcell->get_wwire(), cobj.time_slice, cobj.w_charge, cobj.w_charge_err);
+	// note: sums charge
+	blobmap(cobj.mcell_id, geomcell, cobj.time_slice, cobj.charge);
 
 	clock3 = chrono::system_clock::now();
-
-	in_duration += clock2 - clock1;
-	out_duration += clock3 - clock2;
+	duration2 += clock3 - clock2;
+	duration1 += clock2 - clock1;
     }
+    xwriter.geom.fill();
+    xwriter.frame.fill();
+    xwriter.image.fill();
 
-    int dummy;			// don't care
 
-    // T_true
-    Xdata::Field* field_true = xdatafile.image().new_field(dummy);
-    field_true->name = "true charge deposition";
+    Xdata::Field& field = xwriter.field.obj();
+    Xdata::CloneHelper<Xdata::FieldPoint> fieldca(*field.points);
+
+    field.ident=1;
+    field.trigid = trigger.ident;
+    field.name = "true charge deposition";
+    fieldca.clear();
     nentries = truetree->GetEntries();
     cerr << "Loading " << nentries <<  " entries of T_true" << endl;
     for (auto entry=0; entry<nentries; ++entry) {
@@ -309,18 +374,20 @@ int main(int argc, const char* argv[])
 	truetree->GetEntry(entry);
 	clock2 = chrono::system_clock::now();
 
-	Xdata::FieldPoint fp(Xdata::Point(trueobj.x, trueobj.y, trueobj.z),
-			     vector<float>{(float)trueobj.q});
-	field_true->points.push_back(fp);
+	Xdata::FieldPoint* fp = fieldca.make();
+	fp->point = Xdata::Point(trueobj.x, trueobj.y, trueobj.z);
+	fp->values.push_back(trueobj.q);
 
 	clock3 = chrono::system_clock::now();
-	in_duration += clock2 - clock1;
-	out_duration += clock3 - clock2;
+	duration1 += clock2 - clock1;
+	duration2 += clock3 - clock2;
     }
+    xwriter.field.fill();
 
-
-    Xdata::Field* field_rec = xdatafile.image().new_field(dummy);
-    field_rec->name = "no charge image";
+    field.ident=2;
+    field.trigid = trigger.ident;
+    field.name = "no charge image";
+    fieldca.clear();
     nentries = rectree->GetEntries();
     cerr << "Loading " << nentries <<  " entries of T_rec" << endl;
     for (auto entry=0; entry<nentries; ++entry) {
@@ -328,18 +395,20 @@ int main(int argc, const char* argv[])
 	rectree->GetEntry(entry);
 	clock2 = chrono::system_clock::now();
 
-	Xdata::FieldPoint fp(Xdata::Point(recobj.x, recobj.y, recobj.z),
-			     vector<float>{});
-	field_rec->points.push_back(fp);
+	Xdata::FieldPoint* fp = fieldca.make();
+	fp->point = Xdata::Point(recobj.x, recobj.y, recobj.z);
+	//fp->values.push_back(...);//none...
 
 	clock3 = chrono::system_clock::now();
-	in_duration += clock2 - clock1;
-	out_duration += clock3 - clock2;
+	duration1 += clock2 - clock1;
+	duration2 += clock3 - clock2;
     }
+    xwriter.field.fill();
 
-
-    Xdata::Field* field_recq = xdatafile.image().new_field(dummy);
-    field_recq->name = "charge image";
+    field.ident=3;
+    field.trigid = trigger.ident;
+    field.name = "charge image";
+    fieldca.clear();
     nentries = recqtree->GetEntries();
     cerr << "Loading " << nentries <<  " entries of T_rec_charge" << endl;
     for (auto entry=0; entry<nentries; ++entry) {
@@ -347,61 +416,62 @@ int main(int argc, const char* argv[])
 	recqtree->GetEntry(entry);
 	clock2 = chrono::system_clock::now();
 
-	vector<float> values{
-	    (float)recqobj.q,(float)recqobj.nq,(float)recqobj.chi2,(float)recqobj.ndf
-		};
-	Xdata::FieldPoint fp(Xdata::Point(recqobj.x, recqobj.y, recqobj.z), values);
-	field_recq->points.push_back(fp);
+	Xdata::FieldPoint* fp = fieldca.make();
+	fp->point = Xdata::Point(recqobj.x, recqobj.y, recqobj.z);
+	fp->values.push_back(recqobj.q);
+	fp->values.push_back(recqobj.nq);
+	fp->values.push_back(recqobj.chi2);
+	fp->values.push_back(recqobj.ndf);
 
 	clock3 = chrono::system_clock::now();
-	in_duration += clock2 - clock1;
-	out_duration += clock3 - clock2;
+	duration1 += clock2 - clock1;
+	duration2 += clock3 - clock2;
     }
+    xwriter.field.fill();
 
-    
-    cerr << "shower3d read time: " << in_duration.count() << "s\n";
-    cerr << "xdata packing time: " << out_duration.count() << "s\n";
+    cerr << "shower3d read time: " << duration1.count() << "s\n";
+    cerr << "xdata packing time: " << duration2.count() << "s\n";
 
     cerr << "Writing xdata:\n"
-	 << "\t" << xdatafile.geom().wires.size() <<  " wires\n"
-	 << "\t" << xdatafile.image().num_decos() <<  " decos\n"
-	 << "\t" << xdatafile.image().num_cells() <<  " cells\n"
-	 << "\t" << xdatafile.image().num_blobs() <<  " blobs\n"
-	 << "\t" << xdatafile.image().num_fields() <<  " fields\n";
+	 << "\t" << wireca.size() <<  " wires\n"
+	 << "\t" << cellca.size() <<  " cells\n"
+	 << "\t" << frameca.size() <<  " decos\n"
+	 << "\t" << blobca.size() <<  " blobs\n"
+	 << "\t" << fieldca.size() <<  " fields\n";
 
     clock1 = chrono::system_clock::now();
-    auto siz = xdatafile.write(argv[2]);
+//    auto siz = xdatafile.write(argv[2]);
     clock2 = chrono::system_clock::now();
-    out_duration = clock2 - clock1;
-    cerr << "Write "<<siz<<" bytes xdata in " << out_duration.count() << "s\n";
+    duration2 = clock2 - clock1;
+    cerr << "Write xdata file in " << duration2.count() << "s\n";
 
     
-    {				// do a readback test....
-	cerr << "Test readback:" << endl;
-	clock1 = chrono::system_clock::now();
-	Xdata::XdataFile readback;
-	readback.read(argv[2]);
-	clock2 = chrono::system_clock::now();
-	in_duration = clock2 - clock1;
-	cerr << "... in " << in_duration.count() << "s\n";
+    // {				// do a readback test....
+    // 	cerr << "Test readback:" << endl;
+    // 	clock1 = chrono::system_clock::now();
+    // 	Xdata::XdataFile readback;
+    // 	readback.read(argv[2]);
+    // 	clock2 = chrono::system_clock::now();
+    // 	in_duration = clock2 - clock1;
+    // 	cerr << "... in " << in_duration.count() << "s\n";
 
-	Xdata::RunInfo& ri = readback.runinfo();
-	uint64_t rs = ri.ident;
-	uint64_t run64 = (rs>>32);
-	uint64_t subrun64 = (rs&0xFFFFFFFF);
-	cerr << "Run="  << run64 << " subrun:" << subrun64 << endl;
-	cerr << "Detector: " << ri.detector << endl;
-	cerr << "Read xdata:\n"
-	     << "\t" << readback.geom().wires.size() <<  " wires\n"
-	     << "\t" << readback.image().num_decos() <<  " decos\n"
-	     << "\t" << readback.image().num_cells() <<  " cells\n"
-	     << "\t" << readback.image().num_blobs() <<  " blobs\n"
-	     << "\t" << readback.image().num_fields() <<  " fields\n";
+    // 	Xdata::RunInfo& ri = readback.runinfo();
+    // 	uint64_t rs = ri.ident;
+    // 	uint64_t run64 = (rs>>32);
+    // 	uint64_t subrun64 = (rs&0xFFFFFFFF);
+    // 	cerr << "Run="  << run64 << " subrun:" << subrun64 << endl;
+    // 	cerr << "Detector: " << ri.detector << endl;
+    // 	cerr << "Read xdata:\n"
+    // 	     << "\t" << readback.geom().wires.size() <<  " wires\n"
+    // 	     << "\t" << readback.image().num_decos() <<  " decos\n"
+    // 	     << "\t" << readback.image().num_cells() <<  " cells\n"
+    // 	     << "\t" << readback.image().num_blobs() <<  " blobs\n"
+    // 	     << "\t" << readback.image().num_fields() <<  " fields\n";
 
-	if (ri.detector == "") {
-	    cerr << "ERROR: failed to read back detector name" << endl;
-	}
-    }    
+    // 	if (ri.detector == "") {
+    // 	    cerr << "ERROR: failed to read back detector name" << endl;
+    // 	}
+    // }    
     return 0;
 
 }
