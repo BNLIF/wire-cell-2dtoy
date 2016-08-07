084387dd5f2a2095e9f6a29a8b8e8d0ce0cdaeb7
Xin Qian <last.qx@gmail.com>
2016-08-07 14:31:07 -0400

    update the dead area calculation

--- apps/wire-cell-real-data.cxx
+++ apps/wire-cell-real-data.cxx
@@ -352,7 +352,7 @@ int main(int argc, char* argv[])
    
     
     if (i==0){
-      badtiling[i] = new WireCell2dToy::BadTiling(i,nrebin,uplane_map,vplane_map,wplane_map,gds,1); // 2 plane bad tiling
+      badtiling[i] = new WireCell2dToy::BadTiling(0,i,nrebin,uplane_map,vplane_map,wplane_map,gds,1); // 2 plane bad tiling
       // badtiling[i] = new WireCell2dToy::BadTiling(1,i,nrebin,uplane_map,vplane_map,wplane_map,gds,1); // 1 plane bad tiling
     }
 
