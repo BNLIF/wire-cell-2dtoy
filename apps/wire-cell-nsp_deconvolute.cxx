46d047c6397f78981b2248c5f6675203846be5e2
Xin Qian <last.qx@gmail.com>
2016-08-21 15:43:44 -0400

    add a new application to do deconvolution

---
+++ apps/wire-cell-nsp_deconvolute.cxx
@@ -0,0 +1,105 @@
+#include "WireCellSst/GeomDataSource.h"
+#include "WireCellSst/DatauBooNEFrameDataSource.h"
+#include "WireCellSst/ToyuBooNESliceDataSource.h"
+#include "WireCell2dToy/ToyEventDisplay.h"
+#include "WireCell2dToy/ToyTiling.h"
+#include "WireCell2dToy/BadTiling.h"
+
+#include "WireCell2dToy/MergeToyTiling.h"
+#include "WireCell2dToy/TruthToyTiling.h"
+#include "WireCell2dToy/SimpleBlobToyTiling.h"
+
+#include "WireCell2dToy/ToyMatrix.h"
+#include "WireCell2dToy/ToyMatrixExclusive.h"
+#include "WireCell2dToy/ToyMatrixKalman.h"
+#include "WireCell2dToy/ToyMatrixIterate.h"
+#include "WireCell2dToy/ToyMatrixIterate_SingleWire.h"
+#include "WireCell2dToy/ToyMatrixIterate_Only.h"
+
+
+#include "WireCell2dToy/ToyMatrixMarkov.h"
+#include "WireCell2dToy/ToyMetric.h"
+#include "WireCell2dToy/BlobMetric.h"
+
+#include "WireCellData/MergeGeomCell.h"
+#include "WireCellData/MergeGeomWire.h"
+
+#include "WireCellData/GeomCluster.h"
+
+
+#include "WireCellNav/FrameDataSource.h"
+#include "WireCellNav/SimDataSource.h"
+#include "WireCellNav/SliceDataSource.h"
+#include "WireCellSst/Util.h"
+#include "WireCellData/SimTruth.h"
+#include "WireCell2dToy/ToyDepositor.h"
+#include "WireCellNav/GenerativeFDS.h"
+#include "WireCell2dToy/ToySignalSimu.h"
+#include "WireCell2dToy/ToySignalSimuTrue.h"
+
+#include "WireCell2dToy/DataSignalGaus.h"
+#include "WireCell2dToy/DataSignalWien_ROI.h"
+
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TH1F.h"
+#include "TFile.h"
+#include "TGraph2D.h"
+#include "TColor.h"
+#include "TVectorD.h"
+#include "TMatrixD.h"
+#include <iostream>
+
+using namespace WireCell;
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+  if (argc < 3) {
+    cerr << "usage: wire-cell-uboone /path/to/ChannelWireGeometry.txt /path/to/celltree.root " << endl;
+    return 1;
+  }
+
+  WireCellSst::GeomDataSource gds(argv[1]);
+  std::vector<double> ex = gds.extent();
+  cerr << "Extent: "
+       << " x:" << ex[0]/units::mm << " mm"
+       << " y:" << ex[1]/units::m << " m"
+       << " z:" << ex[2]/units::m << " m"
+       << endl;
+  cout << "Pitch: " << gds.pitch(WirePlaneType_t(0)) 
+       << " " << gds.pitch(WirePlaneType_t(1)) 
+       << " " << gds.pitch(WirePlaneType_t(2))
+       << endl;
+  cout << "Angle: " << gds.angle(WirePlaneType_t(0)) 
+       << " " << gds.angle(WirePlaneType_t(1)) 
+       << " " << gds.angle(WirePlaneType_t(2))
+       << endl;
+
+  TString filename = argv[2];
+  TFile *file = new TFile(filename);
+  TTree *Trun = (TTree*)file->Get("Trun");
+  TTree *T_bad = (TTree*)file->Get("T_bad");
+  TH2F *hu_raw = (TH2F*)file->Get("hu_raw");
+  TH2F *hv_raw = (TH2F*)file->Get("hv_raw");
+  TH2F *hw_raw = (TH2F*)file->Get("hw_raw");
+  
+  WireCellSst::DatauBooNEFrameDataSource data_fds(hu_raw,hv_raw,hw_raw,T_bad,Trun,gds);
+
+
+  //data_fds.jump(0);
+
+  int run_no = data_fds.get_run_no();
+  int subrun_no = data_fds.get_subrun_no();
+  int event_no = data_fds.get_event_no();
+  cout << "Run No: " << run_no << " " << subrun_no << " " << event_no << endl;
+  
+  ChirpMap& uplane_map = data_fds.get_u_cmap();
+  ChirpMap& vplane_map = data_fds.get_v_cmap();
+  ChirpMap& wplane_map = data_fds.get_w_cmap();
+  cout << "Bad Channels: " << uplane_map.size() << " " << vplane_map.size() << " " << wplane_map.size() << endl;
+
+
+}
