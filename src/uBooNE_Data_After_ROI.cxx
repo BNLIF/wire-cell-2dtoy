205a75ffa9ccb000a13b1a5be5ea044407999009
Xin Qian <last.qx@gmail.com>
2016-09-03 09:58:02 -0400

    prepare for work

--- src/uBooNE_Data_After_ROI.cxx
+++ src/uBooNE_Data_After_ROI.cxx
@@ -120,7 +120,6 @@ int WireCell2dToy::uBooNEDataAfterROI::jump(int frame_number){
   int nticks = fds.Get_Bins_Per_Frame();
 
   TH1F *htemp_signal = new TH1F("htemp_signal","htemp_signal",nticks,0,nticks);
-  TH1F *htemp_flag = new TH1F("htemp_flag","htemp_flag",nticks,0,nticks);
   
   for (size_t ind =0; ind<ntraces; ++ind) {
     htemp_signal->Reset();
@@ -506,65 +505,53 @@ int WireCell2dToy::uBooNEDataAfterROI::jump(int frame_number){
 	
       }
     }
-
-    
   }
 
-
-  std::cout << rois_u_tight.size() << " " << rois_v_tight.size() << " " << rois_w_tight.size() << " " << rois_u_loose.size() << " " << rois_v_loose.size() << " " << rois_w_loose.size() << " " << front_rois.size() << " " << back_rois.size() << " " << contained_rois.size() << std::endl;
-
-  
-
-
-  std::vector<std::pair<int,int>>& uboone_rois = rois.get_loose_rois(0);
+  // std::cout << rois_u_tight.size() << " " << rois_v_tight.size() << " " << rois_w_tight.size() << " " << rois_u_loose.size() << " " << rois_v_loose.size() << " " << rois_w_loose.size() << " " << front_rois.size() << " " << back_rois.size() << " " << contained_rois.size() << std::endl;
 
   
+  
+
+  // load results back into the data ... 
   
   for (size_t ind=0; ind<ntraces; ++ind) {
     htemp_signal->Reset();
-    htemp_flag->Reset();
     
     const Trace& trace = frame1.traces[ind];
     int tbin = trace.tbin;
     int chid = trace.chid;
     int nbins = trace.charge.size();
 
-    //copy signal
-    for (int i = tbin;i!=tbin+nbins;i++){
-      int tt = i+1;
-      htemp_signal->SetBinContent(tt,trace.charge.at(i));
-    }
-    
-    //set ROIs
-    uboone_rois = rois.get_loose_rois(chid);
-   
-
-    // std::cout << htemp_signal->GetBinContent(100) << " " << uboone_rois.size() << std::endl;
-    
-    for (int i=0;i!=uboone_rois.size();i++){
-      for (int j=uboone_rois.at(i).first; j<=uboone_rois.at(i).second; j++){
-	htemp_flag->SetBinContent(j+1,1);
+    // load results back into the histogram
+    if (chid < nwire_u){
+      for (auto it = rois_u_loose.at(chid).begin(); it!= rois_u_loose.at(chid).end();it++){
+	SignalROI *roi =  *it;
+	std::vector<float>& contents = roi->get_contents();
+	int start_bin = roi->get_start_bin();
+	for (int i=0;i!=contents.size();i++){
+	  htemp_signal->SetBinContent(start_bin+1+i,contents.at(i));
+	}
+      }
+    }else if (chid < nwire_u + nwire_v){
+      for (auto it = rois_v_loose.at(chid-nwire_u).begin(); it!= rois_v_loose.at(chid-nwire_u).end();it++){
+      	SignalROI *roi =  *it;
+      	std::vector<float>& contents = roi->get_contents();
+      	int start_bin = roi->get_start_bin();
+      	for (int i=0;i!=contents.size();i++){
+      	  htemp_signal->SetBinContent(start_bin+1+i,contents.at(i));
+      	}
+      }
+    }else{
+      for (auto it = rois_w_tight.at(chid-nwire_u-nwire_v).begin(); it!= rois_w_tight.at(chid-nwire_u-nwire_v).end();it++){
+      	SignalROI *roi =  *it;
+      	std::vector<float>& contents = roi->get_contents();
+      	int start_bin = roi->get_start_bin();
+      	for (int i=0;i!=contents.size();i++){
+      	  htemp_signal->SetBinContent(start_bin+1+i,contents.at(i));
+      	}
       }
     }
-    //remove outside ROIs
-    for (int i=0;i!=htemp_signal->GetNbinsX();i++){
-      if (htemp_flag->GetBinContent(i+1)==0)
-	htemp_signal->SetBinContent(i+1,0);
-    }
-
-    //apply adaptive baseline ... 
-    for (int i=0;i!=uboone_rois.size();i++){
-      int start_bin = uboone_rois.at(i).first;
-      int end_bin = uboone_rois.at(i).second;
-      float start_content = htemp_signal->GetBinContent(start_bin+1);
-      float end_content = htemp_signal->GetBinContent(end_bin+1);
-
-      for (int j=start_bin;j<=end_bin;j++){
-	float content = start_content + (j-start_bin) * 1.0 /(end_bin - start_bin) * (end_content-start_content);
-	htemp_signal->SetBinContent(j+1,htemp_signal->GetBinContent(j+1) - content);
-      }
-    }
-
+        
     // save into frames ... 
     WireCell::Trace trace1;
     trace1.chid = chid;
@@ -575,16 +562,13 @@ int WireCell2dToy::uBooNEDataAfterROI::jump(int frame_number){
       float sum = 0;
       for (int j=0;j!=rebin;j++){
 	sum += htemp_signal->GetBinContent(i*rebin+j+1);
-	//sum += htemp_flag->GetBinContent(i*rebin+j+1);
       }
       trace1.charge.at(i) = sum;
-      //      std::cout << rebin << " " << i << " " << sum << std::endl;
     }
     frame.traces.push_back(trace1);
   }
   
   delete htemp_signal;
-  delete htemp_flag;
 
   frame.index = frame_number;
   return frame.index;
