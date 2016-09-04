979e81ff20c681a9e9fcdac20c559ca315192085
Xin Qian <last.qx@gmail.com>
2016-09-04 12:36:11 -0400

    working on more separating ROIs

--- src/uBooNE_Data_After_ROI.cxx
+++ src/uBooNE_Data_After_ROI.cxx
@@ -288,23 +288,18 @@ void WireCell2dToy::uBooNEDataAfterROI::BreakROI(SignalROI* roi, float rms){
   for (Int_t i=0;i!=htemp->GetNbinsX();i++){
     htemp->SetBinContent(i+1,contents.at(i));
   }
-  TSpectrum *s = new TSpectrum(100);
+  TSpectrum *s = new TSpectrum(200);
   Int_t nfound = s->Search(htemp,2,"nobackground new",0.1);
-
-  Int_t flag_single_peak = 0;
-
   float th_peak = 3.0;
-  float tail_th = 2.5;
-  float th_1d = 3.0;
-    
+  float sep_peak = 8.0;
+  
   std::set<int> saved_boundaries;
 
   if (nfound > 1){
     Int_t npeaks = s->GetNPeaks();
     Double_t *peak_pos = s->GetPositionX();
     Double_t *peak_height = s->GetPositionY();
-    
-    int order_peak_pos[105];
+    int order_peak_pos[205];
     int npeaks_threshold = 0;
     for (Int_t j=0;j!=npeaks;j++){
       order_peak_pos[j] = *(peak_pos+j);
@@ -312,141 +307,128 @@ void WireCell2dToy::uBooNEDataAfterROI::BreakROI(SignalROI* roi, float rms){
 	npeaks_threshold ++;
       }
     }
-
     if (npeaks_threshold >1){
       std::sort(order_peak_pos,order_peak_pos + npeaks);
-      
-      float valley_pos[100];
+      float valley_pos[205];
       valley_pos[0] = start_bin;
-      
+      float min = 1e9;
+      for (int k=0; k< order_peak_pos[0]-start_bin;k++){
+	if (htemp->GetBinContent(k+1) < min){
+	  min = htemp->GetBinContent(k+1);
+	    valley_pos[0] = k+start_bin;
+	}
+      }
       for (Int_t j=0;j!=npeaks-1;j++){
 	Float_t min = 1e9;
+	valley_pos[j+1] = order_peak_pos[j];
 	for (Int_t k = order_peak_pos[j]-start_bin; k< order_peak_pos[j+1]-start_bin;k++){
-	  
 	  if (htemp->GetBinContent(k+1) < min){
 	    min = htemp->GetBinContent(k+1);
 	    valley_pos[j+1] = k+start_bin;
 	  }
 	}
-	//	std::cout << valley_pos[j+1] << std::endl;
-	//std::cout << *(peak_pos+j) << std::endl;
       }
-      
       valley_pos[npeaks] = end_bin;
-      
-     
-      
-      for (Int_t j=0;j!=npeaks;j++){
-	int flag = 0;
-	Int_t start_pos = valley_pos[j];
-	Double_t start_content = htemp->GetBinContent(valley_pos[j]-start_bin+1);
-	Int_t end_pos = valley_pos[j+1];
-	Double_t end_content = htemp->GetBinContent(valley_pos[j+1]-start_bin+1);
-	Int_t Peak_pos = order_peak_pos[j];
-	Double_t peak_content = htemp->GetBinContent(order_peak_pos[j]-start_bin+1);
-      
-	if ((start_content > rms || end_content > rms) &&
-	    start_content < peak_content /2. && end_content < peak_content/2.) flag = 1;
-	
-	// deal with the small peaks ... 
-	if (peak_content > rms && peak_content < rms * 1.5) flag =1;
-	
-	if (flag==1){
-	  saved_boundaries.insert(start_pos);
-	  saved_boundaries.insert(end_pos);
-	  //	  std::cout << start_pos << " " << start_content << " " << end_pos << 
-	  //  " " << end_content << std::endl;
+      min = 1e9;
+      for (int k=order_peak_pos[npeaks-1]-start_bin; k<= end_bin-start_bin;k++){
+	if (htemp->GetBinContent(k+1) < min){
+	  min = htemp->GetBinContent(k+1);
+	  valley_pos[npeaks] = k+start_bin;
 	}
       }
       
-
-      TH1F *htemp1 = (TH1F*)htemp->Clone("htemp1");
-      for (Int_t j=0;j!=npeaks;j++){
-	int flag = 0;
-	Int_t start_pos = valley_pos[j];
-	Double_t start_content = htemp1->GetBinContent(valley_pos[j]-start_bin+1);
-	Int_t end_pos = valley_pos[j+1];
-	Double_t end_content = htemp1->GetBinContent(valley_pos[j+1]-start_bin+1);
-	Int_t Peak_pos = order_peak_pos[j];
-	Double_t peak_content = htemp1->GetBinContent(order_peak_pos[j]-start_bin+1);
+      if (roi->get_chid() == 1209 && roi->get_plane() == WirePlaneType_t(0)){
+      	for (int j=0;j!=npeaks;j++){
+      	  std::cout << valley_pos[j] << " " << htemp->GetBinContent(valley_pos[j]-start_bin+1)<< " " << order_peak_pos[j] << " " << htemp->GetBinContent( order_peak_pos[j]-start_bin+1) << " " << valley_pos[j+1] << " " << htemp->GetBinContent(valley_pos[j+1] - start_bin+1)<< std::endl ;
+      	}
+      }
 	
+
+      
+      // need to organize the peaks and valleys ... 
+      float valley_pos1[205];
+      float peak_pos1[205];
+      int npeaks1 = 0;
+      // fill in the first valley;
+      valley_pos1[0] = valley_pos[0];
+      for (int j=0;j<npeaks;j++){
+	if (npeaks1 >0){
+	  // find the lowest valley except the first peak, except the first one
+	  if (htemp->GetBinContent(valley_pos[j]-start_bin+1)< htemp->GetBinContent(valley_pos1[npeaks1]-start_bin+1)){
+	    valley_pos1[npeaks1] = valley_pos[j];
+	  }
+	}
+
+	// find the next peak
+	if ( htemp->GetBinContent(order_peak_pos[j]-start_bin+1) - htemp->GetBinContent(valley_pos1[npeaks1]-start_bin+1) > rms * sep_peak){
+	  peak_pos1[npeaks1] = order_peak_pos[j] ;
+	  npeaks1 ++;
+	  int flag1 = 0;
+
+	  for (int k=j+1;k!=npeaks+1;k++){
+	    // find the highest peak before ... 
+	    if (k<=npeaks){
+	      if (htemp->GetBinContent(order_peak_pos[k-1]-start_bin+1) > htemp->GetBinContent(peak_pos1[npeaks1-1]-start_bin+1))
+		peak_pos1[npeaks1-1] = order_peak_pos[k-1];
+	    }
+
+	    if (htemp->GetBinContent(peak_pos1[npeaks1-1]-start_bin+1) - htemp->GetBinContent(valley_pos[k]-start_bin+1) > rms * sep_peak){
+	      valley_pos1[npeaks1] = valley_pos[k];
+	      j = k-1;
+	      flag1 = 1;
+	      break;
+	    }
+	    // find the next valley
+	  }
+	  if (flag1 == 0){
+	    valley_pos1[npeaks1] = valley_pos[npeaks];
+	    j = npeaks;
+	  }
+
+	  if (roi->get_chid() == 1209 && roi->get_plane() == WirePlaneType_t(0))
+	    std::cout << "c: " << npeaks << " " << valley_pos1[npeaks1-1] << " " << peak_pos1[npeaks1-1] << " " << valley_pos1[npeaks1] << " " << rms * sep_peak << std::endl;
+	}
+      }
+      // fill the last valley
+      valley_pos1[npeaks1] = valley_pos[npeaks];
+      //std::cout << roi->get_plane() << " " << roi->get_chid() << " " << npeaks << " " << npeaks1 << " " ;
+      //std::cout << start_bin << " " << end_bin << std::endl;
+
+      for (Int_t j=0;j!=npeaks1;j++){
+	Int_t start_pos = valley_pos1[j];
+	Int_t end_pos = valley_pos1[j+1];
+	//std::cout << "b " << start_pos << " " << end_pos << std::endl;
+	saved_boundaries.insert(start_pos);
+	saved_boundaries.insert(end_pos);
+      }
+            
+      TH1F *htemp1 = (TH1F*)htemp->Clone("htemp1");
+      htemp->Reset();
+      for (Int_t j=0;j!=npeaks1;j++){
+	int flag = 0;
+	Int_t start_pos = valley_pos1[j];
+	Double_t start_content = htemp1->GetBinContent(valley_pos1[j]-start_bin+1);
+	Int_t end_pos = valley_pos1[j+1];
+	Double_t end_content = htemp1->GetBinContent(valley_pos1[j+1]-start_bin+1);
+	
+	//	std::cout << "a " << start_pos << " " << end_pos << std::endl;
+
 	if (saved_boundaries.find(start_pos) != saved_boundaries.end() ||
 	    saved_boundaries.find(end_pos) != saved_boundaries.end()){
-	  
-	  //std::cout << j << std::endl;
-	  
-	  float max = 0;
-	  float ave = 0;
-	  float ave1 = 0;
-	  
+	  if (roi->get_chid() == 1209 && roi->get_plane() == WirePlaneType_t(0))
+	    std::cout << "d: " << start_pos << " " << end_pos << std::endl;
+
 	  for (Int_t k = start_pos; k!=end_pos+1;k++){
 	    Double_t temp_content = htemp1->GetBinContent(k-start_bin+1) - (start_content + (end_content-start_content) * (k-start_pos) / (end_pos - start_pos));
-	    if (temp_content > rms){
-	      ave += temp_content;
-	      ave1 += 1;
-	    }
-	    if (temp_content > max)
-	      max = temp_content;
-	    //	      htemp->SetBinContent(k-begin+1,temp_content);
+	    htemp->SetBinContent(k-start_bin+1,temp_content);
 	  }
-	  //std::cout << "abc: " << start_pos << " " << end_pos << " " << max << " " << ave << " " << ave1 << std::endl;
-	  
-	  if (max *ave1 > tail_th*ave){
-	    //std::cout << "abc: " << start_pos << " " << end_pos << " " << max << " " << ave << " " << ave1 << std::endl;
-	    for (Int_t k = start_pos; k!=end_pos+1;k++){
-	      Double_t temp_content = htemp1->GetBinContent(k-start_bin+1) - (start_content + (end_content-start_content) * (k-start_pos) / (end_pos - start_pos));
-	      if (temp_content > th_1d*rms){
-		htemp->SetBinContent(k-start_bin+1,temp_content);
-	      }else{
-		htemp->SetBinContent(k-start_bin+1,0);
-	      }
-	    }
-	  }else{
-	    for (Int_t k = start_pos; k!=end_pos+1;k++){
-	      Double_t temp_content = htemp1->GetBinContent(k-start_bin+1) - (start_content + (end_content-start_content) * (k-start_pos) / (end_pos - start_pos));
-	      htemp->SetBinContent(k-start_bin+1,temp_content);
-	    }
-	  }
-	  
-	     
 	}
       }
       delete htemp1;
-
-    } else{
-      flag_single_peak = 1;
     }
-  } else{
-    flag_single_peak = 1;
-  }
-
-  if (flag_single_peak == 1){
-    // find maximum and average
-    float max = 0;
-    float ave = 0;
-    float ave1 = 0;
-    for (Int_t j=0;j!=htemp->GetNbinsX();j++){
-      if (htemp->GetBinContent(j+1) > rms){
-  	ave += htemp->GetBinContent(j+1);
-  	ave1 += 1;
-      }
-      if (htemp->GetBinContent(j+1) > max){
-  	max = htemp->GetBinContent(j+1);
-      }
-    }
-    
-    if (max *ave1 > tail_th*ave){
-      for (Int_t j=0;j!=htemp->GetNbinsX();j++){
-  	if (htemp->GetBinContent(j+1) < th_1d*rms){
-  	  htemp->SetBinContent(j+1,0);
-  	}
-      }
-    }
-    
   }
   
    for (Int_t i=0;i!=htemp->GetNbinsX();i++){
-     //htemp->SetBinContent(i+1,contents.at(i));
      contents.at(i) = htemp->GetBinContent(i+1);
    }
   
