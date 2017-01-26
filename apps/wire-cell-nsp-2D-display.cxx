641584c7efdcab7e13840a1c67abd47e322ba254
Xin Qian <last.qx@gmail.com>
2017-01-26 03:00:00 -0500

    change back to rebin

--- apps/wire-cell-nsp-2D-display.cxx
+++ apps/wire-cell-nsp-2D-display.cxx
@@ -90,7 +90,7 @@ int main(int argc, char* argv[])
   float toffset_3=0.0;
   int total_time_bin=9594;
   int frame_length = 3200;
-  int nrebin = 1;
+  int nrebin = 6;
   int eve_num  = atoi(argv[3]);
   int time_offset = -92.;
   
