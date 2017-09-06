62652d4f97cad59c9461b86ed57d18d340e93265
Xin Qian <last.qx@gmail.com>
2017-09-06 13:49:28 -0400

    add some code for L1SP

--- inc/WireCell2dToy/uBooNE_L1SP.h
+++ inc/WireCell2dToy/uBooNE_L1SP.h
@@ -3,7 +3,7 @@
 
 #include "WireCellData/GeomWire.h"
 #include "TH2F.h"
-
+#include "TGraph.h"
 
 
 namespace WireCell2dToy{
@@ -14,9 +14,14 @@ namespace WireCell2dToy{
 
     void AddWires(int time_slice, WireCell::GeomWireSelection& wires);
     void Form_rois(int pad = 2);
+    
+    void L1_fit(int wire_index, int start_tick, int end_tick);
+    
   protected:
     TH2F *hv_raw;
     TH2F *hv_decon;
+    TGraph *gv, *gw;
+    
     int nrebin;
     // wire index --> time
     std::map<int,std::vector<int>> init_map;
--- src/uBooNE_L1SP.cxx
+++ src/uBooNE_L1SP.cxx
@@ -4,6 +4,7 @@
 #include "WireCellRess/ElasticNetModel.h"
 #include <Eigen/Dense>
 
+
 using namespace Eigen;
 using namespace WireCell;
 
@@ -12,9 +13,66 @@ WireCell2dToy::uBooNE_L1SP::uBooNE_L1SP(TH2F *hv_raw, TH2F *hv_decon, int nrebin
   , hv_decon(hv_decon)
   , nrebin(nrebin)
 {
+  #include "data_70_2D_11.txt"
+  TGraph **gv_2D_g = new TGraph*[11];
+  gv_2D_g[0] = new TGraph(5000,v_2D_g_0_x,v_2D_g_0_y);
+  gv_2D_g[1] = new TGraph(5000,v_2D_g_1_x,v_2D_g_1_y);
+  gv_2D_g[2] = new TGraph(5000,v_2D_g_2_x,v_2D_g_2_y);
+  gv_2D_g[3] = new TGraph(5000,v_2D_g_3_x,v_2D_g_3_y);
+  gv_2D_g[4] = new TGraph(5000,v_2D_g_4_x,v_2D_g_4_y);
+  gv_2D_g[5] = new TGraph(5000,v_2D_g_5_x,v_2D_g_5_y);
+  gv_2D_g[6] = new TGraph(5000,v_2D_g_6_x,v_2D_g_6_y);
+  gv_2D_g[7] = new TGraph(5000,v_2D_g_7_x,v_2D_g_7_y);
+  gv_2D_g[8] = new TGraph(5000,v_2D_g_8_x,v_2D_g_8_y);
+  gv_2D_g[9] = new TGraph(5000,v_2D_g_9_x,v_2D_g_9_y);
+  gv_2D_g[10] = new TGraph(5000,v_2D_g_10_x,v_2D_g_10_y);
+
+  TGraph **gw_2D_g = new TGraph*[11];
+  gw_2D_g[0] = new TGraph(5000,w_2D_g_0_x,w_2D_g_0_y);
+  gw_2D_g[1] = new TGraph(5000,w_2D_g_1_x,w_2D_g_1_y);
+  gw_2D_g[2] = new TGraph(5000,w_2D_g_2_x,w_2D_g_2_y);
+  gw_2D_g[3] = new TGraph(5000,w_2D_g_3_x,w_2D_g_3_y);
+  gw_2D_g[4] = new TGraph(5000,w_2D_g_4_x,w_2D_g_4_y);
+  gw_2D_g[5] = new TGraph(5000,w_2D_g_5_x,w_2D_g_5_y);
+  gw_2D_g[6] = new TGraph(5000,w_2D_g_6_x,w_2D_g_6_y);
+  gw_2D_g[7] = new TGraph(5000,w_2D_g_7_x,w_2D_g_7_y);
+  gw_2D_g[8] = new TGraph(5000,w_2D_g_8_x,w_2D_g_8_y);
+  gw_2D_g[9] = new TGraph(5000,w_2D_g_9_x,w_2D_g_9_y);
+  gw_2D_g[10] = new TGraph(5000,w_2D_g_10_x,w_2D_g_10_y);
+
+  gv = new TGraph();
+  gw = new TGraph();
+  
+  int scaling = 14 * 1.2 * 4096/2000.;
+  
+  for (Int_t i=0;i!=gv_2D_g[0]->GetN();i++){
+    Double_t x,y;
+    gv_2D_g[0]->GetPoint(i,x,y);
+    for (Int_t j=1;j!=11;j++){
+      y += gv_2D_g[j]->Eval(x) * 2;
+    }
+    y = y * scaling;
+    gv->SetPoint(i,x,y);
+    
+    y = gw_2D_g[0]->Eval(x+3.0);
+    for (Int_t j=1;j!=11;j++){
+      y += gw_2D_g[j]->Eval(x+3.0) * 2;
+    }
+    y = y * scaling;
+    gw->SetPoint(i,x,y);
+  }
+
+  for (int i=0;i!=11;i++){
+    delete gv_2D_g[i];
+    delete gw_2D_g[i];
+  }
+  delete [] gv_2D_g;
+  delete [] gw_2D_g;
 }
 
-WireCell2dToy::uBooNE_L1SP::~uBooNE_L1SP(){ 
+WireCell2dToy::uBooNE_L1SP::~uBooNE_L1SP(){
+  delete gv;
+  delete gw;
 }
 
 void WireCell2dToy::uBooNE_L1SP::AddWires(int time_slice, GeomWireSelection& wires){
@@ -84,7 +142,7 @@ void WireCell2dToy::uBooNE_L1SP::Form_rois(int pad){
       //std::cout << start_bin << " " << end_bin << std::endl;
     }
     
-    std::cout << wire_index << " " << rois.size() << " ";    
+    // std::cout << wire_index << " " << rois.size() << " ";    
     // merge them ...
     for (auto it = rois.begin(); it!= rois.end();  it++){
       if (rois_save.size()==0){
@@ -95,8 +153,57 @@ void WireCell2dToy::uBooNE_L1SP::Form_rois(int pad){
 	rois_save.push_back(*it);
       }
     }
+
+    for (auto it = rois_save.begin(); it!=rois_save.end(); it++){
+      L1_fit(wire_index, it->first * nrebin, it->second * nrebin + nrebin);
+      //   std::cout << wire_index << " " << it->first * nrebin << " " << it->second * nrebin + nrebin <<  " " << (it->second - it->first + 1) * nrebin << std::endl;
+    }
     
     // std::cout << wire_index << " " << rois_save.size() << std::endl;
     // std::cout << wire_index << " " << time_slices.size() << " " << time_slices.front() << " " << time_slices.back() << " " << hv_decon->GetBinContent(wire_index+1,time_slices.front()+1)<< std::endl;
   }
+}
+
+
+void WireCell2dToy::uBooNE_L1SP::L1_fit(int wire_index, int start_tick, int end_tick){
+  const int nbin_fit = end_tick-start_tick;
+  // fill the data ... 
+  VectorXd W = VectorXd::Zero(nbin_fit);
+  for (int i=0; i!= nbin_fit; i++){
+    W(i) = hv_raw->GetBinContent(wire_index+1,start_tick+i+1);
+  }
+
+  std::cout << nbin_fit << std::endl;
+  
+  //for matrix G
+  MatrixXd G = MatrixXd::Zero(nbin_fit, nbin_fit*2);
+  for (int i=0;i!=nbin_fit;i++){ 
+    // X 
+    Double_t t1 = i/2.; // us, measured time  
+    for (int j=0;j!=nbin_fit;j++){
+      // Y ... 
+      Double_t t2 = j/2.; // us, real signal time
+      double delta_t = t1 - t2;
+      if (delta_t >-84 && delta_t < 15.8){
+	G(i,j) = gw->Eval(delta_t) *500;
+	G(i,nbin_fit+j) = gv->Eval(delta_t)*500; 
+      }
+    }
+  }
+
+  double lambda = 0.1;
+  WireCell::LassoModel m2(lambda, 100000, 1e-2);
+  m2.SetData(G, W);
+  m2.Fit();
+  
+  VectorXd beta = m2.Getbeta();
+  
+  double sum1 = 0;
+  double sum2 = 0;
+
+  for (int i=0;i!=nbin_fit;i++){
+    sum1 += beta(i);
+    sum2 += beta(nbin_fit+i);
+  }
+  std::cout << sum1 << " " << sum2 << std::endl;
 }
