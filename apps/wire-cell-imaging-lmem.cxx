aea8da63b4d9db49830f8f303227b384d32acf4b
Xin Qian <last.qx@gmail.com>
2017-09-10 18:49:56 -0400

    fix a bug

--- apps/wire-cell-imaging-lmem.cxx
+++ apps/wire-cell-imaging-lmem.cxx
@@ -283,9 +283,6 @@ int main(int argc, char* argv[])
   TH2F *hv_decon = (TH2F*)file1->Get("hv_decon");
   TH2F *hw_decon = (TH2F*)file1->Get("hw_decon");
   
-  WireCell2dToy::pdDataFDS roi_fds(gds,hu_decon,hv_decon,hw_decon,eve_num);
-  roi_fds.jump(eve_num);
-
   TH2F *hu_decon_g = (TH2F*)file1->Get("hu_decon_g");
   TH2F *hv_decon_g = (TH2F*)file1->Get("hv_decon_g");
   TH2F *hw_decon_g = (TH2F*)file1->Get("hw_decon_g");
@@ -326,8 +323,8 @@ int main(int argc, char* argv[])
   }
   //
 
-
-
+  WireCell2dToy::pdDataFDS roi_fds(gds,hu_decon,hv_decon,hw_decon,eve_num);
+  roi_fds.jump(eve_num);
   
   TH2F *hv_raw = (TH2F*)file1->Get("hv_raw");
   
