cc30877ac244376114e1965d02c17e4489de6cbb
Xin Qian <last.qx@gmail.com>
2017-09-29 11:50:47 -0400

    add a class for light processing

---
+++ inc/WireCell2dToy/uBooNE_light_reco.h
@@ -0,0 +1,14 @@
+#ifndef WIRECELL_UBOONE_LIGHT_RECO_H
+#define WIRECELL_UBOONE_LIGHT_RECO_H
+
+
+namespace WireCell2dToy{
+  class uBooNE_light_reco{
+  public:
+    uBooNE_light_reco(const char* root_file, int eve_num);
+    ~uBooNE_light_reco();
+  protected: 
+  };
+}
+
+#endif
---
+++ src/uBooNE_light_reco.cxx
@@ -0,0 +1,12 @@
+#include "WireCell2dToy/uBooNE_light_reco.h"
+
+//using namespace WireCell;
+
+
+WireCell2dToy::uBooNE_light_reco::uBooNE_light_reco(const char* root_file, int eve_num){
+}
+
+
+WireCell2dToy::uBooNE_light_reco::~uBooNE_light_reco(){
+
+}
