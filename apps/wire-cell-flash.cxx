dac414c6c2ecfdfd428ecdad994582ca6e5f4f10
Xin Qian <last.qx@gmail.com>
2017-09-29 11:38:22 -0400

    add flash

---
+++ apps/wire-cell-flash.cxx
@@ -0,0 +1,41 @@
+#include "WireCellSst/GeomDataSource.h"
+
+#include "TH1F.h"
+
+#include <iostream>
+
+using namespace WireCell;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if (argc < 4) {
+    cerr << "usage: wire-cell-uboone /path/to/ChannelWireGeometry.txt /path/to/celltree.root eve_num " << endl;
+    return 1;
+  }
+  TH1::AddDirectory(kFALSE);
+
+  WireCellSst::GeomDataSource gds(argv[1]);
+  std::vector<double> ex = gds.extent();
+  cerr << "Extent: "
+       << " x:" << ex[0]/units::mm << " mm"
+       << " y:" << ex[1]/units::m << " m"
+       << " z:" << ex[2]/units::m << " m"
+       << endl;
+
+  cout << "Pitch: " << gds.pitch(WirePlaneType_t(0)) 
+       << " " << gds.pitch(WirePlaneType_t(1)) 
+       << " " << gds.pitch(WirePlaneType_t(2))
+       << endl;
+  cout << "Angle: " << gds.angle(WirePlaneType_t(0)) 
+       << " " << gds.angle(WirePlaneType_t(1)) 
+       << " " << gds.angle(WirePlaneType_t(2))
+       << endl;
+
+  const char* root_file = argv[2];
+  int eve_num = atoi(argv[3]);
+
+  
+  
+  return 1;
+}
