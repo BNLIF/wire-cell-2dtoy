637ff219300cbbd8aa4709693aa290d76e10d695
Xin Qian <last.qx@gmail.com>
2017-10-18 15:04:09 -0400

    prepare to add a new class

---
+++ inc/WireCell2dToy/ToyPointCloud.h
@@ -0,0 +1,14 @@
+#ifndef WireCell2dToy_TOYPOINTCLOUD_H
+#define WireCell2dToy_TOYPOINTCLOUD_H
+
+
+//#include "WireCellNanoflann/nanoflann.h" 
+#include "WireCellData/WCPointCloud.h"
+//#include "WireCellQuickhull/QuickHull.h"
+//#include "WireCellQuickhull/MathUtils.h"
+
+namespace WireCell2dToy{
+  
+}
+
+#endif
