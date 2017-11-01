66f9802087cdeb8fba4f26dcbf84d58d5e7895fe
Xin Qian <last.qx@gmail.com>
2017-10-31 22:25:18 -0400

    bug fix

--- src/ToyMatching.cxx
+++ src/ToyMatching.cxx
@@ -18,9 +18,12 @@ int WireCell2dToy::convert_xyz_voxel_id(WireCell::Point &p){
   // int voxel_y_id = std::round((p.y/units::cm+193-5.14667/2.)/5.14667);
   // int voxel_z_id = std::round((p.z/units::cm+128.243-3.23122/2.)/3.23122);
   
-  int voxel_x_id = std::round((p.x/units::cm+64.825-5.14667/2.)/5.14667);
-  int voxel_y_id = std::round((p.y/units::cm+188.416-5.11698/2.)/5.11698);
-  int voxel_z_id = std::round((p.z/units::cm+88.3554-3.05768/2.)/3.05768);
+  int voxel_x_id = std::round((p.x/units::cm+63.435-5.1096/2.)/5.1095);
+  int voxel_y_id = std::round((p.y/units::cm+191.61-5.1096/2.)/5.1096);
+  int voxel_z_id = std::round((p.z/units::cm+92.375-3.05437/2.)/3.05437);
+  //fit
+  // int voxel_y_id = std::round((p.y/units::cm+188.416-5.11698/2.)/5.11698);
+  // int voxel_z_id = std::round((p.z/units::cm+88.3554-3.05768/2.)/3.05768);
   
   if (voxel_x_id<0) voxel_x_id=0;
   if (voxel_x_id>=75) voxel_x_id=74;
@@ -119,6 +122,7 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
     Opflash *flash = (*it1);
     double offset_x = (flash->get_time() - time_offset)*2./nrebin*time_slice_width;
     int cluster_id = 0;
+
     bool flag_good_flash = false;
     
     for (auto it2 = group_clusters.begin(); it2!=group_clusters.end(); it2++){
@@ -129,6 +133,11 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
       double last_pos_x = (*((main_cluster->get_time_cells_set_map().rbegin())->second.begin()))->get_sampling_points().front().x;
 
       bool flag_at_x_boundary = false;
+
+      if (flash_num==57){
+	std::cout << cluster_id << " " << (first_pos_x-offset_x)/units::cm << " " << (last_pos_x-offset_x)/units::cm << " " << flash_num << std::endl;
+      }
+      
       if (first_pos_x-offset_x > low_x_cut + low_x_cut_ext1 &&
 	  last_pos_x-offset_x > low_x_cut &&
 	  last_pos_x-offset_x < high_x_cut + high_x_cut_ext1 &&
@@ -143,7 +152,6 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
 	  std::vector<bool> temp_vec_flag;
 	  map_flash_tpc_boundaries[flash] = temp_vec_flag;
 	}
-	
 	
 	// tracks ends at boundary ... 
 	if (first_pos_x-offset_x <=low_x_cut && first_pos_x-offset_x > low_x_cut + low_x_cut_ext1 ||
@@ -194,13 +202,16 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
 	  }
 	}
 
+	// double sum1 = 0, sum2 = 0;
 	for (size_t i=0;i!=32;i++){
 	  pred_pmt_light.at(i) *= scaling_light_mag;
+	//   sum1 += flash->get_PE(i);
+	//   sum2 += pred_pmt_light.at(i);
 	}
 
+	// if (fabs(sum2/sum1-1)<0.75){
 	map_flash_tpc_ids[flash].push_back(cluster_id);
 	map_flash_tpc_light_preds[flash].push_back(pred_pmt_light);
-	
 	// fill in the content ... 
 	if (flag_at_x_boundary){
 	  map_flash_tpc_boundaries[flash].push_back(true);
@@ -210,6 +221,7 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
 	}else{
 	  map_flash_tpc_boundaries[flash].push_back(false);
 	}
+	// }
       }
       cluster_id++;
     }
@@ -223,48 +235,56 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
 
   
   // regularization strength ... 
-  double lambda = 32;
+  double lambda = 0.25; // note the coefficient is all around 1
   //form matrix ...
-  double fudge_factor = 0.05; // add 5% relative uncertainty for pe
+  double fudge_factor1 = 0.05; // add 5% relative uncertainty for pe
+  double fudge_factor2 = 1.5; // double the original uncertainties
   int num_unknowns = 0;
+  int test_flash_id = 27;
   
   for (auto it = map_flash_tpc_ids.begin(); it!= map_flash_tpc_ids.end(); it++){
-    num_unknowns += it->second.size();
+    //  if (map_flash_index[it->first]==test_flash_id)
+      num_unknowns += it->second.size();
   }
+  //std::cout << num_unknowns << std::endl;
   
-  VectorXd W = VectorXd::Zero(32*good_flashes.size());
-  MatrixXd G = MatrixXd::Zero(32*good_flashes.size(), num_unknowns);
+
+  // let's try to fit one flash only ... 
+  VectorXd W = VectorXd::Zero(33*good_flashes.size());
+  MatrixXd G = MatrixXd::Zero(33*good_flashes.size(), num_unknowns);
   std::vector<std::pair<Opflash*,int>> total_pairs;
   std::vector<double> total_weights;
   
   for (size_t i=0; i!= good_flashes.size(); i++){
     Opflash *flash = good_flashes.at(i);
-    for (size_t j=0;j!=32;j++){
-      double pe = flash->get_PE(j);
-      double pe_err = sqrt(pow(flash->get_PE_err(j),2) + pow(pe*fudge_factor,2));
-      W(32*i+j) = pe/pe_err;
-    }
-    for (size_t j=0;j!=map_flash_tpc_ids[flash].size();j++){
-   
-      for (size_t k=0;k!=32;k++){
-	double pe = flash->get_PE(k);
-	double pe_err = sqrt(pow(flash->get_PE_err(k),2) + pow(pe*fudge_factor,2));
-	G(32*i+k,total_pairs.end()-total_pairs.begin()) = 1./pe_err * map_flash_tpc_light_preds[flash].at(j).at(map_pmt_lib[k]);
+    {
+    //    if (map_flash_index[flash]==test_flash_id){
+      for (size_t j=0;j!=32;j++){
+	double pe = flash->get_PE(j);
+	double pe_err = sqrt(pow(flash->get_PE_err(j)*fudge_factor2,2) + pow(pe*fudge_factor1,2));
+	W(33*i+j) = pe/pe_err;
       }
-      total_pairs.push_back(std::make_pair(flash,map_flash_tpc_ids[flash].at(j)));
-      if (map_flash_tpc_boundaries[flash].at(j)){ // add boundary ... 
-	total_weights.push_back(0.1);
-      }else{
-	total_weights.push_back(1);
+      W(33*i+32)=6.7; // 15%
+      for (size_t j=0;j!=map_flash_tpc_ids[flash].size();j++){
+	for (size_t k=0;k!=32;k++){
+	  double pe = flash->get_PE(k);
+	  double pe_err = sqrt(pow(flash->get_PE_err(k)*fudge_factor2,2) + pow(pe*fudge_factor1,2));
+	  G(33*i+k,total_pairs.end()-total_pairs.begin()) = 1./pe_err * map_flash_tpc_light_preds[flash].at(j).at(map_pmt_lib[k]);
+	}
+	G(33*i+32,total_pairs.end()-total_pairs.begin()) = 6.7; //15%
+	
+	total_pairs.push_back(std::make_pair(flash,map_flash_tpc_ids[flash].at(j)));
+	if (map_flash_tpc_boundaries[flash].at(j)){ // add boundary ... 
+	  //total_weights.push_back(1.0);
+	  total_weights.push_back(0.25);
+	}else{
+	  total_weights.push_back(1);
+	}
       }
     }
   }
-
-
-  // std::cout << 32*good_flashes.size() << " " << num_unknowns << " " << total_weights.size() << " " << total_pairs.size() << std::endl;
   
-
-  WireCell::LassoModel m2(lambda, 100000, 0.05);
+  WireCell::LassoModel m2(lambda, 100000, 0.01);
   m2.SetData(G, W);
   for (size_t i=0; i!=total_weights.size(); i++){
     m2.SetLambdaWeight(i,total_weights.at(i));
@@ -272,22 +292,90 @@ void WireCell2dToy::tpc_light_match(int time_offset, int nrebin, std::map<WireCe
   m2.Fit();
   VectorXd beta = m2.Getbeta();
   for (size_t i=0;i!=total_pairs.size();i++){
-   std::cout << i << " " <<  map_flash_index[total_pairs.at(i).first] << " " << total_pairs.at(i).second << " " << total_weights.at(i) << " " << beta(i)  << std::endl;
+    if(beta(i)!=0)
+      std::cout << i << " " <<  map_flash_index[total_pairs.at(i).first] << " " << total_pairs.at(i).second << " " << total_weights.at(i) << " " << beta(i)  << std::endl;
   }
+  // double chi2_1 = 0, chi2_2 = 0;
+  // for (size_t i=0;i!=33;i++){
+  //   chi2_1 += pow(W(i)-G(i,14),2);
+  //   double sum = 0;
+  //   for (size_t j=0;j!=15;j++){
+  //     sum += G(i,j)*beta(j);
+  //   }
+  //   std::cout << i << " " << W(i) << " " << G(i,14) << " " << total_weights.at(14) <<" " <<   sum  << " " << std::endl;
+  //   chi2_2 += pow(W(i) - sum,2);
+  // }
+  // std::cout << chi2_1 << " "<< chi2_2 << std::endl;
 
-  // for (size_t i=0;i!=32;i++){
-  //   std::cout << i << " " << W(32*4+i) << " " << G(32*4+i,60) << " " << total_weights.at(60) << std::endl;
+
+
+
+
+  
+
+  // for (auto it = map_flash_tpc_ids.begin(); it!= map_flash_tpc_ids.end(); it++){
+  //   num_unknowns += it->second.size();
   // }
   
+  // VectorXd W = VectorXd::Zero(32*good_flashes.size());
+  // MatrixXd G = MatrixXd::Zero(32*good_flashes.size(), num_unknowns);
+  // std::vector<std::pair<Opflash*,int>> total_pairs;
+  // std::vector<double> total_weights;
   
-  // number of unknonws are matched pairs #matched_pair < num_flashes * tpc_object
-  // Matrix would then be (32*num_flashes, #matched_pair)
-  // for each coefficient, we need to save its flash and the tpc_object
-  // for each tpc object, we need to save all its coefficients, so we can pick up the latest one
-  
-  // For each TPC object matched with a particular, need to generate 
+  // for (size_t i=0; i!= good_flashes.size(); i++){
+  //   Opflash *flash = good_flashes.at(i);
+  //   for (size_t j=0;j!=32;j++){
+  //     double pe = flash->get_PE(j);
+  //     double pe_err = sqrt(pow(flash->get_PE_err(j),2) + pow(pe*fudge_factor,2));
+  //     W(32*i+j) = pe/pe_err;
+  //   }
+  //   for (size_t j=0;j!=map_flash_tpc_ids[flash].size();j++){
+   
+  //     for (size_t k=0;k!=32;k++){
+  // 	double pe = flash->get_PE(k);
+  // 	double pe_err = sqrt(pow(flash->get_PE_err(k),2) + pow(pe*fudge_factor,2));
+  // 	G(32*i+k,total_pairs.end()-total_pairs.begin()) = 1./pe_err * map_flash_tpc_light_preds[flash].at(j).at(map_pmt_lib[k]);
+  //     }
+  //     total_pairs.push_back(std::make_pair(flash,map_flash_tpc_ids[flash].at(j)));
+  //     if (map_flash_tpc_boundaries[flash].at(j)){ // add boundary ... 
+  // 	total_weights.push_back(1.0);
+  // 	//total_weights.push_back(0.1);
+  //     }else{
+  // 	total_weights.push_back(1);
+  //     }
+  //   }
+  // }
 
 
+  // // std::cout << 32*good_flashes.size() << " " << num_unknowns << " " << total_weights.size() << " " << total_pairs.size() << std::endl;
+  
+
+  // WireCell::LassoModel m2(lambda, 100000, 1e-3);
+  // m2.SetData(G, W);
+  // for (size_t i=0; i!=total_weights.size(); i++){
+  //   m2.SetLambdaWeight(i,total_weights.at(i));
+  // }
+  // m2.Fit();
+
+  // VectorXd beta = m2.Getbeta();
+  // for (size_t i=0;i!=total_pairs.size();i++){
+  //   if (map_flash_index[total_pairs.at(i).first]  == 4)
+  //  std::cout << i << " " <<  map_flash_index[total_pairs.at(i).first] << " " << total_pairs.at(i).second << " " << total_weights.at(i) << " " << beta(i)  << std::endl;
+  // }
+
+  // double chi2_1 = 0, chi2_2 = 0;
+  // for (size_t i=0;i!=32;i++){
+  //   chi2_1 += pow(W(32*4+i)-G(32*4+i,60),2);
+  
+  //   double sum = 0;
+  //   for (size_t j=46;j!=61;j++){
+  //     sum += G(32*4+i,j)*beta(j);
+  //   }
+  //   std::cout << i << " " << W(32*4+i) << " " << G(32*4+i,60) << " " << total_weights.at(60) <<" " <<   sum  << " " << std::endl;
+  //   chi2_2 += pow(W(32*4+i) - sum,2);
+  // }
+  // std::cout << chi2_1 << " "<< chi2_2 << std::endl;
+  
   
   
 }
