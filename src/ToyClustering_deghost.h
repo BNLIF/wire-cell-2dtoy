3e6fea61abfd45ad9de83cc091f50880c6fc374f
Xin Qian <last.qx@gmail.com>
2018-01-08 14:25:03 -0500

    add some codes

--- src/ToyClustering_deghost.h
+++ src/ToyClustering_deghost.h
@@ -35,6 +35,9 @@ void WireCell2dToy::Clustering_deghost(WireCell::PR3DClusterSelection& live_clus
   
   DynamicToyPointCloud global_point_cloud(angle_u,angle_v,angle_w);
   DynamicToyPointCloud global_skeleton_cloud(angle_u,angle_v,angle_w);
+
+  std::vector<PR3DCluster*> to_be_removed_clusters;
+  std::set<std::pair<PR3DCluster*, PR3DCluster*>> to_be_merged_pairs;
   
   for (size_t i=0;i!=live_clusters.size();i++){
     if (i==0){
@@ -47,34 +50,111 @@ void WireCell2dToy::Clustering_deghost(WireCell::PR3DClusterSelection& live_clus
       }
     }else{ 
       // start the process to add things in and perform deghosting ... 
-
       
+      // two cases, merge clusters or remove clusters
       
     }
   }
 
-  Point p(0,0,0);
-
-  std::tuple<double, PR3DCluster*, size_t> test1 = global_point_cloud.get_closest_point_info(p);
-  std::tuple<double, PR3DCluster*, size_t> test2 = global_point_cloud.get_closest_2d_point_info(p,0);
-  std::tuple<double, PR3DCluster*, size_t> test3 = global_point_cloud.get_closest_2d_point_info(p,1);
-  std::tuple<double, PR3DCluster*, size_t> test4 = global_point_cloud.get_closest_2d_point_info(p,2);
-
-  std::cout << std::get<0>(test1)/units::cm << " " << std::get<1>(test1)->get_cluster_id() << " "
-	    << std::get<0>(test2)/units::cm << " " << std::get<1>(test2)->get_cluster_id() << " "
-	    << std::get<0>(test3)/units::cm << " " << std::get<1>(test3)->get_cluster_id() << " "
-	    << std::get<0>(test4)/units::cm << " " << std::get<1>(test4)->get_cluster_id() << " "
-	    << std::endl;
 
 
-  test1 = global_skeleton_cloud.get_closest_point_info(p);
-  test2 = global_skeleton_cloud.get_closest_2d_point_info(p,0);
-  test3 = global_skeleton_cloud.get_closest_2d_point_info(p,1);
-  test4 = global_skeleton_cloud.get_closest_2d_point_info(p,2);
+  //merge clusters
+  std::vector<std::set<PR3DCluster*>> merge_clusters;
+  for (auto it = to_be_merged_pairs.begin(); it!=to_be_merged_pairs.end(); it++){
+    PR3DCluster *cluster1 = (*it).first;
+    PR3DCluster *cluster2 = (*it).second;
+    //  std::cout << cluster1 << " " << cluster2 << " " << cluster1->get_cluster_id() << " " << cluster2->get_cluster_id() << std::endl;
+    
+    bool flag_new = true;
+    std::vector<std::set<PR3DCluster*>> temp_set;
+    for (auto it1 = merge_clusters.begin(); it1!=merge_clusters.end(); it1++){
+      std::set<PR3DCluster*>& clusters = (*it1);
+      if (clusters.find(cluster1)!=clusters.end() ||
+	  clusters.find(cluster2)!=clusters.end()){
+	clusters.insert(cluster1);
+	clusters.insert(cluster2);
+	flag_new = false;
+	temp_set.push_back(clusters);
+	//break;
+      }
+    }
+    if (flag_new){
+      std::set<PR3DCluster*> clusters;
+      clusters.insert(cluster1);
+      clusters.insert(cluster2);
+      merge_clusters.push_back(clusters);
+    }
+    if (temp_set.size()>1){
+      // merge them further ...
+      std::set<PR3DCluster*> clusters;
+      for (size_t i=0;i!=temp_set.size();i++){
+	for (auto it1 = temp_set.at(i).begin(); it1!= temp_set.at(i).end(); it1++){
+	  clusters.insert(*it1);
+	}
+	merge_clusters.erase(find(merge_clusters.begin(),merge_clusters.end(),temp_set.at(i)));
+      }
+      merge_clusters.push_back(clusters);
+    }
+  }
+
+  // merge clusters into new clusters, delete old clusters 
+  for (auto it = merge_clusters.begin(); it!=merge_clusters.end();it++){
+    std::set<PR3DCluster*>& clusters = (*it);
+    PR3DCluster *ncluster = new PR3DCluster((*clusters.begin())->get_cluster_id());
+    live_clusters.push_back(ncluster);
+    for (auto it1 = clusters.begin(); it1!=clusters.end(); it1++){
+      PR3DCluster *ocluster = *(it1);
+      // std::cout << ocluster->get_cluster_id() << " ";
+      SMGCSelection& mcells = ocluster->get_mcells();
+      for (auto it2 = mcells.begin(); it2!=mcells.end(); it2++){
+  	SlimMergeGeomCell *mcell = (*it2);
+  	//std::cout << ocluster->get_cluster_id() << " " << mcell << std::endl;
+  	int time_slice = mcell->GetTimeSlice();
+  	ncluster->AddCell(mcell,time_slice);
+      }
+      live_clusters.erase(find(live_clusters.begin(), live_clusters.end(), ocluster));
+      cluster_length_map.erase(ocluster);
+      delete ocluster;
+    }
+    std::vector<int> range_v1 = ncluster->get_uvwt_range();
+    double length_1 = sqrt(2./3. * (pow(pitch_u*range_v1.at(0),2) + pow(pitch_v*range_v1.at(1),2) + pow(pitch_w*range_v1.at(2),2)) + pow(time_slice_width*range_v1.at(3),2));
+    cluster_length_map[ncluster] = length_1;
+    //std::cout << std::endl;
+  }
   
-  std::cout << std::get<0>(test1)/units::cm << " " << std::get<1>(test1)->get_cluster_id() << " "
-	    << std::get<0>(test2)/units::cm << " " << std::get<1>(test2)->get_cluster_id() << " "
-	    << std::get<0>(test3)/units::cm << " " << std::get<1>(test3)->get_cluster_id() << " "
-	    << std::get<0>(test4)/units::cm << " " << std::get<1>(test4)->get_cluster_id() << " "
-	    << std::endl;
+  // delete clusters ... 
+  for (auto it = to_be_removed_clusters.begin(); it!=to_be_removed_clusters.end(); it++){
+    PR3DCluster *ocluster = *it;
+    live_clusters.erase(find(live_clusters.begin(), live_clusters.end(), ocluster));
+    cluster_length_map.erase(ocluster);
+    delete ocluster;
+  }
+
+  
+
+  
+  /* Point p(0,0,0); */
+
+  /* std::tuple<double, PR3DCluster*, size_t> test1 = global_point_cloud.get_closest_point_info(p); */
+  /* std::tuple<double, PR3DCluster*, size_t> test2 = global_point_cloud.get_closest_2d_point_info(p,0); */
+  /* std::tuple<double, PR3DCluster*, size_t> test3 = global_point_cloud.get_closest_2d_point_info(p,1); */
+  /* std::tuple<double, PR3DCluster*, size_t> test4 = global_point_cloud.get_closest_2d_point_info(p,2); */
+
+  /* std::cout << std::get<0>(test1)/units::cm << " " << std::get<1>(test1)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test2)/units::cm << " " << std::get<1>(test2)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test3)/units::cm << " " << std::get<1>(test3)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test4)/units::cm << " " << std::get<1>(test4)->get_cluster_id() << " " */
+  /* 	    << std::endl; */
+
+
+  /* test1 = global_skeleton_cloud.get_closest_point_info(p); */
+  /* test2 = global_skeleton_cloud.get_closest_2d_point_info(p,0); */
+  /* test3 = global_skeleton_cloud.get_closest_2d_point_info(p,1); */
+  /* test4 = global_skeleton_cloud.get_closest_2d_point_info(p,2); */
+  
+  /* std::cout << std::get<0>(test1)/units::cm << " " << std::get<1>(test1)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test2)/units::cm << " " << std::get<1>(test2)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test3)/units::cm << " " << std::get<1>(test3)->get_cluster_id() << " " */
+  /* 	    << std::get<0>(test4)/units::cm << " " << std::get<1>(test4)->get_cluster_id() << " " */
+  /* 	    << std::endl; */
 }
