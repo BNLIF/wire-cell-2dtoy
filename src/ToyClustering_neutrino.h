dfca508ce4af82643fc43a1b776114da502f2be1
Xin Qian <last.qx@gmail.com>
2018-01-27 22:19:36 -0500

    add more code

--- src/ToyClustering_neutrino.h
+++ src/ToyClustering_neutrino.h
@@ -10,6 +10,19 @@ void WireCell2dToy::Clustering_neutrino(WireCell::PR3DClusterSelection& live_clu
   double time_slice_width = mp.get_ts_width();
 
 
+   // sort the clusters length ...
+  {
+    std::vector<std::pair<PR3DCluster*,double>> temp_pair_vec;
+    for (auto it = cluster_length_map.begin(); it!=cluster_length_map.end();it++){
+      temp_pair_vec.push_back(std::make_pair(it->first, it->second));
+    }
+    sort(temp_pair_vec.begin(), temp_pair_vec.end(), sortbysec);
+    live_clusters.clear();
+    for (auto it = temp_pair_vec.begin(); it!=temp_pair_vec.end();it++){
+      live_clusters.push_back(it->first);
+    }
+  }
+  
   // find all the clusters that are inside the box ...
   WireCell::PR3DClusterSelection contained_clusters;
   WireCell::PR3DClusterSelection candidate_clusters;
