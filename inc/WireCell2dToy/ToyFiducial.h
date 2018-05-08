f2b2d7bb04fcd5222125fb73336bf46dbc224817
Xin Qian <last.qx@gmail.com>
2018-05-08 10:59:54 -0400

    update code

--- inc/WireCell2dToy/ToyFiducial.h
+++ inc/WireCell2dToy/ToyFiducial.h
@@ -1,8 +1,43 @@
 #ifndef WIRECELL_UBOONE_TOYFIDUCIAL_H
 #define WIRECELL_UBOONE_TOYFIDUCIAL_H
 
+#include "WireCellData/Units.h"
+
 namespace WireCell2dToy{
-  
+  class ToyFiducial{
+  public:
+    ToyFiducial(double top=117*units::cm, double bottom=-116*units::cm, double upstream=0*units::cm, double downstream=1037*units::cm, double anode = 0*units::cm, double cathode=256*units::cm);
+    ~ToyFiducial();
+
+    
+    //void AddDeadRegion();
+    
+  protected:
+    // boundary
+    double m_top; // top distance
+    double m_bottom; // bottom distance
+    double m_upstream;
+    double m_downstream;
+    double m_anode;
+    double m_cathode;
+    
+    // space charge boundary
+    double m_sc_bottom_1_x, m_sc_bottom_1_y;
+    double m_sc_bottom_2_x, m_sc_bottom_2_y;
+
+    double m_sc_top_1_x, m_sc_top_1_y;
+    double m_sc_top_2_x, m_sc_top_2_y;
+
+    double m_sc_upstream_1_x, m_sc_upstream_1_z;
+    double m_sc_upstream_2_x, m_sc_upstream_2_z;
+
+    double m_sc_downstream_1_x, m_sc_downstream_1_z;
+    double m_sc_downstream_2_x, m_sc_downstream_2_z;
+    
+    // dead regions
+
+    
+  };
 }
 
 #endif
--- src/ToyFiducial.cxx
+++ src/ToyFiducial.cxx
@@ -1,1 +1,39 @@
 #include "WireCell2dToy/ToyFiducial.h"
+
+
+
+WireCell2dToy::ToyFiducial::ToyFiducial(double top, double bottom, double upstream, double downstream, double anode, double cathode)
+  : m_top(top)
+  , m_bottom(bottom)
+  , m_upstream(upstream)
+  , m_downstream(downstream)
+  , m_anode(anode)
+  , m_cathode(cathode)
+{
+  m_sc_bottom_1_y=-116*units::cm;
+  m_sc_bottom_1_x=80*units::cm;
+
+  m_sc_bottom_2_y=-99*units::cm;
+  m_sc_bottom_2_x=256*units::cm;
+
+  m_sc_top_1_y = 118*units::cm;
+  m_sc_top_1_x = 100*units::cm;
+
+  m_sc_top_2_y = 103*units::cm;
+  m_sc_top_2_x = 256*units::cm;
+
+  m_sc_upstream_1_z = 0*units::cm;
+  m_sc_upstream_1_x = 120*units::cm;
+
+  m_sc_upstream_2_z = 11*units::cm;
+  m_sc_upstream_2_x = 256*units::cm;
+
+  m_sc_downstream_1_z=1037*units::cm;
+  m_sc_downstream_1_x=120*units::cm;
+
+  m_sc_downstream_2_z=1026*units::cm;
+  m_sc_downstream_2_x=256*units::cm;
+}
+
+WireCell2dToy::ToyFiducial::~ToyFiducial(){
+}
