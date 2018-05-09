dd64451de2a39a190a07e62288cc229dbee051be
Xin Qian <last.qx@gmail.com>
2018-05-09 14:57:14 -0400

    update the code

--- inc/WireCell2dToy/ToyFiducial.h
+++ inc/WireCell2dToy/ToyFiducial.h
@@ -11,12 +11,13 @@
 namespace WireCell2dToy{
   class ToyFiducial{
   public:
-    ToyFiducial(double boundary_dis_cut=3*units::cm, double top=117*units::cm, double bottom=-116*units::cm, double upstream=0*units::cm, double downstream=1037*units::cm, double anode = 0*units::cm, double cathode=256*units::cm);
+    ToyFiducial(int dead_region_ch_ext = 3, double offset_t=800, double offset_u=0, double offset_v=0, double offset_w=0, double slope_t=1./2*units::mm, double slope_u=1./3*units::mm, double slope_v=1./3*units::mm, double slope_w=1./3*units::mm, double angle_u=-1.047198, double angle=1.047198, double angle_w=0,
+		double boundary_dis_cut=3*units::cm, double top=117*units::cm, double bottom=-116*units::cm, double upstream=0*units::cm, double downstream=1037*units::cm, double anode = 0*units::cm, double cathode=256*units::cm);
     ~ToyFiducial();
 
-    bool inside_fiducial_volume(WireCell::Point& p);
+    bool inside_fiducial_volume(WireCell::Point& p, double offset_x=0);
     
-    //void AddDeadRegion();
+    void AddDeadRegion(WireCell::SlimMergeGeomCell* mcell, std::vector<int>& time_slices);
     
   protected:
     // boundary
@@ -46,9 +47,23 @@ namespace WireCell2dToy{
     // dead regions ... 
     WireCell::SMGCSelection mcells;
     std::map<WireCell::SlimMergeGeomCell*, std::pair<int,int>> mcell_time_map;
+    std::map<int, std::set<WireCell::SlimMergeGeomCell*>> ch_mcell_set_map;
     
     // conversion between positions to the channel and time ???
-    
+
+    // convert time into a position
+    // (time_slice - offset_t) / slope_t = position_x 
+    double offset_t, slope_t;
+    // convert u wire number into a position
+    // (u_index -offset_u) / slope_u = position_u
+    double offset_u, slope_u;
+    // convert v wire number into a position
+    double offset_v, slope_v;
+    // convert w wire number into a position 
+    double offset_w, slope_w;
+    double angle_u, angle_v, angle_w;
+
+    int dead_region_ch_ext;
     
   };
 }
--- src/ToyFiducial.cxx
+++ src/ToyFiducial.cxx
@@ -1,5 +1,7 @@
 #include "WireCell2dToy/ToyFiducial.h"
 
+using namespace WireCell;
+
 int pnpoly(std::vector<double>& vertx, std::vector<double>& verty, double testx, double testy)
 {
   int i, j, c = 0;
@@ -12,8 +14,20 @@ int pnpoly(std::vector<double>& vertx, std::vector<double>& verty, double testx,
 }
 
 
-WireCell2dToy::ToyFiducial::ToyFiducial(double boundary_dis_cut, double top, double bottom, double upstream, double downstream, double anode, double cathode)
-  : m_top(top)
+WireCell2dToy::ToyFiducial::ToyFiducial(int dead_region_ch_ext, double offset_t, double offset_u, double offset_v, double offset_w, double slope_t, double slope_u, double slope_v, double slope_w, double angle_u, double angle_v, double angle_w, double boundary_dis_cut, double top, double bottom, double upstream, double downstream, double anode, double cathode)
+  : dead_region_ch_ext(dead_region_ch_ext)
+  , offset_t(offset_t)
+  , offset_u(offset_u)
+  , offset_v(offset_v)
+  , offset_w(offset_w)
+  , slope_t(slope_t)
+  , slope_u(slope_u)
+  , slope_v(slope_v)
+  , slope_w(slope_w)
+  , angle_u(angle_u)
+  , angle_v(angle_v)
+  , angle_w(angle_w)
+  , m_top(top)
   , m_bottom(bottom)
   , m_upstream(upstream)
   , m_downstream(downstream)
@@ -68,13 +82,76 @@ WireCell2dToy::ToyFiducial::~ToyFiducial(){
 }
 
 
-bool WireCell2dToy::ToyFiducial::inside_fiducial_volume(WireCell::Point &p){
-  int c1 = pnpoly(boundary_xy_x, boundary_xy_y, p.x, p.y);
-  int c2 = pnpoly(boundary_xz_x, boundary_xz_z, p.x, p.z);
+bool WireCell2dToy::ToyFiducial::inside_fiducial_volume(WireCell::Point &p, double offset_x){
+  int c1 = pnpoly(boundary_xy_x, boundary_xy_y, p.x-offset_x, p.y);
+  int c2 = pnpoly(boundary_xz_x, boundary_xz_z, p.x-offset_x, p.z);
   
   if (c1 && c2){
     return true;
   }else{
     return false;
   }
+}
+
+
+void WireCell2dToy::ToyFiducial::AddDeadRegion(WireCell::SlimMergeGeomCell* mcell, std::vector<int>& time_slices){
+
+  mcells.push_back(mcell);
+  int start_time = time_slices.front() - dead_region_ch_ext ;
+  int end_time = time_slices.back() + dead_region_ch_ext;
+  mcell_time_map[mcell] = std::make_pair(start_time, end_time);
+
+  GeomWireSelection& uwires = mcell->get_uwires();
+  GeomWireSelection& vwires = mcell->get_vwires();
+  GeomWireSelection& wwires = mcell->get_wwires();
+  
+  std::vector<WirePlaneType_t> bad_planes = mcell->get_bad_planes();
+
+  if (find(bad_planes.begin(), bad_planes.end(), WirePlaneType_t(0))!=bad_planes.end()){
+    int start_ch = uwires.front()->channel() - dead_region_ch_ext;
+    if (start_ch <0) start_ch = 0;
+    int end_ch = uwires.back()->channel() + dead_region_ch_ext;
+    if (end_ch>=2400) end_ch = 2399;
+    for (int i = start_ch; i<=end_ch;i++){
+      if (ch_mcell_set_map.find(i)==ch_mcell_set_map.end()){
+	std::set<SlimMergeGeomCell*> mcells_set;
+	mcells_set.insert(mcell);
+	ch_mcell_set_map[i] = mcells_set;
+      }else{
+	ch_mcell_set_map[i].insert(mcell);
+      }
+    }
+  }
+  if (find(bad_planes.begin(), bad_planes.end(), WirePlaneType_t(1))!=bad_planes.end()){
+    int start_ch = vwires.front()->channel() - dead_region_ch_ext;
+    if (start_ch <2400) start_ch = 2400;
+    int end_ch = vwires.back()->channel() + dead_region_ch_ext;
+    if (end_ch>=4800) end_ch = 4799;
+    for (int i = start_ch; i<=end_ch;i++){
+      if (ch_mcell_set_map.find(i)==ch_mcell_set_map.end()){
+	std::set<SlimMergeGeomCell*> mcells_set;
+	mcells_set.insert(mcell);
+	ch_mcell_set_map[i] = mcells_set;
+      }else{
+	ch_mcell_set_map[i].insert(mcell);
+      }
+    }
+  }
+  if (find(bad_planes.begin(), bad_planes.end(), WirePlaneType_t(2))!=bad_planes.end()){
+    int start_ch = wwires.front()->channel() - dead_region_ch_ext;
+    if (start_ch <4800) start_ch = 4800;
+    int end_ch = wwires.back()->channel() + dead_region_ch_ext;
+    if (end_ch>=7256) end_ch = 7255;
+    for (int i = start_ch; i<=end_ch;i++){
+      if (ch_mcell_set_map.find(i)==ch_mcell_set_map.end()){
+	std::set<SlimMergeGeomCell*> mcells_set;
+	mcells_set.insert(mcell);
+	ch_mcell_set_map[i] = mcells_set;
+      }else{
+	ch_mcell_set_map[i].insert(mcell);
+      }
+    }
+  }
+  
+  
 }
