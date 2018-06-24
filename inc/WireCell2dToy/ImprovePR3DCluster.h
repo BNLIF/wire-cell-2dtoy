8bf02e81bc289255d9b41e2528a4834eae67ff0a
Xin Qian <last.qx@gmail.com>
2018-06-24 10:29:33 -0400

    add a new function

---
+++ inc/WireCell2dToy/ImprovePR3DCluster.h
@@ -0,0 +1,10 @@
+#ifndef WIRECELL_IMPROVEPR3DCluster_H
+#define WIRECELL_IMPROVEPR3DCluster_H
+
+#include "WireCellData/PR3DCluster.h"
+
+namespace WireCell2dToy{
+  WireCell::PR3DCluster* Improve_PR3DCluster(WireCell::PR3DCluster* cluster);
+}
+
+#endif 
---
+++ src/ImprovePR3DCluster.cxx
@@ -0,0 +1,7 @@
+#include "WireCell2dToy/ImprovePR3DCluster.h"
+
+using namespace WireCell;
+
+WireCell::PR3DCluster* WireCell2dToy::Improve_PR3DCluster(WireCell::PR3DCluster* cluster){
+  return 0;
+}
