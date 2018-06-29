3b14d2faca2422e4310f3fe4ab3b75ff64da1d93
Xin Qian <last.qx@gmail.com>
2018-06-29 11:32:27 -0400

    update code

--- src/ToyFiducial.cxx
+++ src/ToyFiducial.cxx
@@ -42,10 +42,10 @@ WireCell2dToy::ToyFiducial::ToyFiducial(int dead_region_ch_ext, double offset_t,
   m_sc_bottom_2_y=-99*units::cm;
   m_sc_bottom_2_x=256*units::cm;
 
-  m_sc_top_1_y = 118*units::cm;
+  m_sc_top_1_y = 116*units::cm; // used to be 118 cm
   m_sc_top_1_x = 100*units::cm;
 
-  m_sc_top_2_y = 103*units::cm;
+  m_sc_top_2_y = 102*units::cm; // used to be 103 cm
   m_sc_top_2_x = 256*units::cm;
 
   m_sc_upstream_1_z = 0*units::cm;
@@ -205,16 +205,26 @@ bool WireCell2dToy::ToyFiducial::check_tgm(WireCell::FlashTPCBundle *bundle, dou
       }
       
 
-      // if (main_cluster->get_cluster_id()==13){
-       
+      // if (main_cluster->get_cluster_id()==8){
+      // 	std::cout << main_cluster->get_cluster_id() << " " << i << " " <<
+      // 	  out_vec_wcps.at(i).at(0).x/units::cm << " " << out_vec_wcps.at(i).at(0).y/units::cm << " " << out_vec_wcps.at(i).at(0).z/units::cm << " " << 
+      // 	  k << " " << out_vec_wcps.at(k).at(0).x/units::cm << " " << out_vec_wcps.at(k).at(0).y/units::cm << " " << out_vec_wcps.at(k).at(0).z/units::cm <<
+      // 	  " " << p1_index << " " << p2_index << " " << flag_p1_inside << " " << flag_p2_inside << std::endl;
+      // 	for (size_t j=0;j!=out_vec_wcps.at(i).size();j++){
+      // 	  Point p1(out_vec_wcps.at(i).at(j).x,out_vec_wcps.at(i).at(j).y,out_vec_wcps.at(i).at(j).z);
+      // 	  std::cout << j << " A " << p1.x/units::cm << " " << p1.y/units::cm << " " << p1.z/units::cm << std::endl;
+      // 	}
+      // 	for(size_t j=0;j!=out_vec_wcps.at(k).size();j++){
+      // 	  Point p2(out_vec_wcps.at(k).at(j).x,out_vec_wcps.at(k).at(j).y,out_vec_wcps.at(k).at(j).z);
+      // 	  std::cout << j << " B " << p2.x/units::cm << " " << p2.y/units::cm << " " << p2.z/units::cm << std::endl;
+      // 	}
       // }
-      //  std::cout << main_cluster->get_cluster_id() << " " << i << " " << k << " " << p1_index << " " << p2_index << " " << flag_p1_inside << " " << flag_p2_inside << std::endl;
 	
       if ((!flag_p1_inside) && (!flag_p2_inside)){
 	// if not a neutrino candidate ... to be worked out ...
 	 // Point p1(out_vec_wcps.at(i).at(p1_index).x,out_vec_wcps.at(i).at(p1_index).y,out_vec_wcps.at(i).at(p1_index).z);
 	 // Point p2(out_vec_wcps.at(k).at(p2_index).x,out_vec_wcps.at(k).at(p2_index).y,out_vec_wcps.at(k).at(p2_index).z);
-	 // std::cout << p1.x/units::cm << " " << p1.y/units::cm << " " << p1.z/units::cm << " " << inside_fiducial_volume(p1,offset_x) << " A " << p2.x/units::cm << " " << p2.y/units::cm << " " << p2.z/units::cm << " " << inside_fiducial_volume(p2,offset_x) << " " << offset_x/units::cm << std::endl;
+	//	std::cout << p1.x/units::cm << " " << p1.y/units::cm << " " << p1.z/units::cm << " " << inside_fiducial_volume(p1,offset_x) << " A " << p2.x/units::cm << " " << p2.y/units::cm << " " << p2.z/units::cm << " " << inside_fiducial_volume(p2,offset_x) << " " << offset_x/units::cm << std::endl;
 
 
 	// std::cout << main_cluster->get_cluster_id() << " " << (out_vec_wcps.at(i).at(p1_index).x-offset_x)/units::cm << " " << out_vec_wcps.at(i).at(p1_index).y/units::cm << " " << out_vec_wcps.at(i).at(p1_index).z/units::cm << " " ;
@@ -234,6 +244,11 @@ bool WireCell2dToy::ToyFiducial::check_tgm(WireCell::FlashTPCBundle *bundle, dou
 	
 	if (flag_check){
 	  if (flash->get_type()==2){
+
+	    // if (main_cluster->get_cluster_id()==8){
+	    //   std::cout << check_neutrino_candidate(main_cluster,out_vec_wcps.at(i).at(p1_index),out_vec_wcps.at(k).at(p2_index),offset_x) << std::endl;
+	    // }
+
 	    if (!check_neutrino_candidate(main_cluster,out_vec_wcps.at(i).at(p1_index),out_vec_wcps.at(k).at(p2_index),offset_x))
 	      return true;
 	  }else{
