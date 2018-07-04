9067d31508c20c1860e0b73cfaf91f467c5a986a
Xin Qian <last.qx@gmail.com>
2018-07-04 12:59:50 -0400

    update

--- src/ToyMatching.cxx
+++ src/ToyMatching.cxx
@@ -1123,17 +1123,17 @@ FlashTPCBundleSelection WireCell2dToy::tpc_light_match(int time_offset, int nreb
     
     // // finish further examine the bundle ... 
     
-     std::cout << "After Cleaning 2 : " << cluster_bundles_map.size() << " A " << flash_bundles_map.size() << " " << all_bundles.size() << std::endl;
+    //  std::cout << "After Cleaning 2 : " << cluster_bundles_map.size() << " A " << flash_bundles_map.size() << " " << all_bundles.size() << std::endl;
 
 
-    // std::cout << std::endl << std::endl;
-    for (auto it = all_bundles.begin(); it!=all_bundles.end();it++){
-      FlashTPCBundle *bundle = *it;
+    // // std::cout << std::endl << std::endl;
+    // for (auto it = all_bundles.begin(); it!=all_bundles.end();it++){
+    //   FlashTPCBundle *bundle = *it;
       
-      if ( bundle->get_consistent_flag() || bundle->get_flag_at_x_boundary())
-    	std::cout << bundle->get_flash()->get_flash_id() << " " << bundle->get_main_cluster()->get_cluster_id() << " " << bundle->get_flag_at_x_boundary() << " " << bundle->get_ks_dis() << " " << bundle->get_chi2() << " " << bundle->get_ndf() << " " << bundle->get_consistent_flag()  << std::endl;
-    }
-    std::cout << std::endl << std::endl;
+    //   if ( bundle->get_consistent_flag() || bundle->get_flag_at_x_boundary())
+    // 	std::cout << bundle->get_flash()->get_flash_id() << " " << bundle->get_main_cluster()->get_cluster_id() << " " << bundle->get_flag_at_x_boundary() << " " << bundle->get_ks_dis() << " " << bundle->get_chi2() << " " << bundle->get_ndf() << " " << bundle->get_consistent_flag()  << std::endl;
+    // }
+    // std::cout << std::endl << std::endl;
 
     
     // examining flash ... 
