15c31538d7a06d2341e83ec8c33514117d9c005e
jixiangpan <jixp87@gmail.com>
2018-07-05 14:18:49 -0400

    test track-imaging

--- apps/wire-cell-track-imaging.cxx
+++ apps/wire-cell-track-imaging.cxx
@@ -73,7 +73,7 @@ int main(int argc, char* argv[])
   int cluster_check=-1; // specify cluster_id to check 
   if(argc==4) cluster_check=atoi(argv[3]);
 
-  cout<<" ---> test track-imaging B"<<endl;
+  cout<<" ---> test track-imaging C"<<endl;
 
   return 0;
 
